db1d666d8c5c58482a27191567bd33f1091f818e
Pierre Bertet <hi@bpier.re>
2024-04-09 11:51:15 +0100

    fundAccounts: remove unused code
    It’s probably better to not rely on `accounts[0]` to know if all
    accounts have been funded, since `accounts` is dynamic.

--- contracts/utils/fundAccounts.js
+++ contracts/utils/fundAccounts.js
@@ -1,9 +1,4 @@
 const fundAccounts = async (accounts, token) => {
-  // const areAccountsAlreadyFunded = await token
-  //   .balanceOf(accounts[0])
-  //   .gte(_1e36Str);
-  // if (areAccountsAlreadyFunded) return;
-
   return Promise.all(
     accounts.map((account) => (
       token.mint(account, String(10n ** 36n))
