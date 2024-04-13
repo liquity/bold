ac99d32ba077c6c5f2bea39afdd693b9f6a5432a
Pierre Bertet <hi@bpier.re>
2024-04-13 10:15:02 +0100

    Remove console import

--- contracts/src/scripts/DeployLiquity2.s.sol
+++ contracts/src/scripts/DeployLiquity2.s.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.18;
 
-import "forge-std/console.sol";
 import {Script} from "forge-std/Script.sol";
 import {StdCheats} from "forge-std/StdCheats.sol";
 import "../deployment.sol";
