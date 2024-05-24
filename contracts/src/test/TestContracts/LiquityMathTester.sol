db857750b6e3347fd926b265fa8233fb6d57bf99
ßingen <bingen@liquity.org>
2024-05-24 18:29:24 +0100

    test: Restore skipped tests

--- contracts/src/test/TestContracts/LiquityMathTester.sol
+++ contracts/src/test/TestContracts/LiquityMathTester.sol
@@ -7,12 +7,15 @@ import "../../Dependencies/LiquityMath.sol";
 /* Tester contract for math functions in Math.sol library. */
 
 contract LiquityMathTester {
+    event AvoidWarning(uint256 a);
+
     function callMax(uint256 _a, uint256 _b) external pure returns (uint256) {
         return LiquityMath._max(_a, _b);
     }
 
     // Non-view wrapper for gas test
-    function callDecPowTx(uint256 _base, uint256 _n) external pure returns (uint256) {
+    function callDecPowTx(uint256 _base, uint256 _n) external returns (uint256) {
+        emit AvoidWarning(0);
         return LiquityMath._decPow(_base, _n);
     }
 
