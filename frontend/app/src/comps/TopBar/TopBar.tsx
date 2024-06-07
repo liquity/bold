27a8c3223e3f39e625c3a9987661b0769af3f2e3
Pierre Bertet <hi@bpier.re>
2024-06-08 00:56:56 +0100

    App: remove TopBar drawer

--- frontend/app/src/comps/TopBar/TopBar.tsx
+++ frontend/app/src/comps/TopBar/TopBar.tsx
@@ -1,19 +1,15 @@
 "use client";
 
-import type { MenuSection } from "@/src/types";
 import type { ComponentProps } from "react";
 
 import { Logo } from "@/src/comps/Logo/Logo";
 import content from "@/src/content";
 import { css } from "@/styled-system/css";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useCallback, useEffect, useRef, useState } from "react";
 import { LAYOUT_WIDTH } from "../AppLayout/AppLayout";
 import { AccountButton } from "./AccountButton";
 import { IconBorrow, IconEarn, IconLeverage, IconStake } from "./icons";
 import { Menu } from "./Menu";
-import { MenuDrawer } from "./MenuDrawer";
 
 const menuItems: ComponentProps<typeof Menu>["menuItems"] = [
   [content.menu.borrow, "/borrow", IconBorrow],
@@ -22,115 +18,7 @@ const menuItems: ComponentProps<typeof Menu>["menuItems"] = [
   [content.menu.stake, "/stake", IconStake],
 ];
 
-const menuSections: MenuSection[] = [
-  {
-    actions: [
-      {
-        href: "/borrow/eth",
-        name: "ETH pool",
-        secondary: "91% Max LTV",
-        token: "ETH",
-      },
-      {
-        href: "/borrow/reth",
-        name: "rETH pool",
-        secondary: "91% Max LTV",
-        token: "RETH",
-      },
-      {
-        href: "/borrow/wsteth",
-        name: "wstETH pool",
-        secondary: "91% Max LTV",
-        token: "WSTETH",
-      },
-    ],
-    href: "/borrow",
-    label: content.home.actions.borrow.description,
-  },
-  {
-    actions: [
-      {
-        href: "/leverage/eth",
-        name: "ETH pool",
-        secondary: "91% Max LTV",
-        token: "ETH",
-      },
-      {
-        href: "/leverage/reth",
-        name: "rETH pool",
-        secondary: "91% Max LTV",
-        token: "RETH",
-      },
-      {
-        href: "/leverage/wsteth",
-        name: "wstETH pool",
-        secondary: "91% Max LTV",
-        token: "WSTETH",
-      },
-    ],
-    href: "/leverage",
-    label: content.home.actions.leverage.description,
-  },
-  {
-    actions: [
-      {
-        href: "/earn/eth",
-        name: "ETH pool",
-        secondary: "4.3% APY",
-        token: "ETH",
-      },
-      {
-        href: "/earn/reth",
-        name: "rETH pool",
-        secondary: "6.3% APY",
-        token: "RETH",
-      },
-      {
-        href: "/earn/wsteth",
-        name: "wstETH pool",
-        secondary: "5.1% APY",
-        token: "WSTETH",
-      },
-    ],
-    href: "/earn",
-    label: content.home.actions.earn.description,
-  },
-  {
-    actions: [],
-    href: "/stake",
-    label: content.home.actions.stake.description,
-  },
-];
-
 export function TopBar() {
-  const pathname = usePathname();
-
-  const [hoveredItem, setHoveredItem] = useState(-1);
-
-  const hoverDelay = useRef<ReturnType<typeof setTimeout> | null>(null);
-  const cancelDelayedHover = useCallback(() => {
-    if (!hoverDelay.current) {
-      return;
-    }
-    clearTimeout(hoverDelay.current);
-    hoverDelay.current = null;
-  }, []);
-  const startDelayedHover = useCallback((hoveredItem: number, delay: number) => {
-    cancelDelayedHover();
-    if (delay === 0) {
-      setHoveredItem(hoveredItem);
-      return;
-    }
-    hoverDelay.current = setTimeout(() => {
-      setHoveredItem(hoveredItem);
-    }, delay);
-  }, [cancelDelayedHover]);
-
-  // close the drawer when the route changes
-  useEffect(() => {
-    startDelayedHover(-1, 0);
-  }, [startDelayedHover, pathname]);
-
   return (
     <div
       className={css({
@@ -175,37 +63,8 @@ export function TopBar() {
           <Logo />
           {content.appName}
         </Link>
-        <Menu
-          hovered={hoveredItem}
-          menuItems={menuItems}
-          onHover={(index) => {
-            startDelayedHover(index, hoveredItem === -1 ? 200 : 0);
-          }}
-          onMouseLeave={() => {
-            startDelayedHover(-1, 200);
-          }}
-        />
+        <Menu menuItems={menuItems} />
         <AccountButton />
-      </div>
-
-      <div
-        className={css({
-          position: "absolute",
-          inset: "72px 0 auto",
-          zIndex: 1,
-          height: 0,
-        })}
-      >
-        <MenuDrawer
-          onMouseEnter={() => {
-            cancelDelayedHover();
-          }}
-          onMouseLeave={() => {
-            startDelayedHover(-1, 200);
-          }}
-          opened={hoveredItem}
-          sections={menuSections}
-        />
       </div>
     </div>
   );
