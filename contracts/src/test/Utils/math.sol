27248bf95b6a88d775c9074fccab5e505c55b6a7
Daniel Simon <daniel.attila.simon@gmail.com>
2024-06-11 15:18:58 +0700

    chore: delete duplicated file

--- contracts/src/test/Utils/math.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
-
-function mulDivCeil(uint256 x, uint256 multiplier, uint256 divider) pure returns (uint256) {
-    assert(divider != 0);
-    return x == 0 ? 0 : (x * multiplier + divider - 1) / divider;
-}
