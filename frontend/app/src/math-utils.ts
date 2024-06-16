5766aec87981a9febf5b0995521f76be83a6acc7
Pierre Bertet <hi@bpier.re>
2024-06-16 22:48:07 +0100

    math-utils: naming

--- frontend/app/src/math-utils.ts
+++ frontend/app/src/math-utils.ts
@@ -1,11 +1,11 @@
-export function lerp(value1: number, value2: number, amt: number) {
-  return ((value2 - value1) * amt) + value1;
+export function lerp(start: number, stop: number, value: number) {
+  return ((stop - start) * value) + start;
 }
 
 export function map(value: number, istart: number, istop: number, ostart: number, ostop: number) {
   return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
 }
 
-export function norm(aNumber: number, low: number, high: number) {
-  return (aNumber - low) / (high - low);
+export function norm(value: number, start: number, stop: number) {
+  return (value - start) / (stop - start);
 }
