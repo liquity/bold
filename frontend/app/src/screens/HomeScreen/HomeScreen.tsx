61a3c40b31e53e794cc2e6b10c698479a50a995e
Pierre Bertet <hi@bpier.re>
2024-07-18 19:00:44 +0100

    Home screen: remove debug code

--- frontend/app/src/screens/HomeScreen/HomeScreen.tsx
+++ frontend/app/src/screens/HomeScreen/HomeScreen.tsx
@@ -4,39 +4,14 @@ import { Positions } from "@/src/comps/Positions/Positions";
 import content from "@/src/content";
 import { BORROW_STATS, EARN_POOLS } from "@/src/demo-mode";
 import { DEMO_MODE } from "@/src/env";
-import { useFindAvailableTroveIndex } from "@/src/liquity-utils";
-import { useAccount } from "@/src/services/Ethereum";
 import { usePrice } from "@/src/services/Prices";
-import { useTransactionFlow } from "@/src/services/TransactionFlow";
 import { infoTooltipProps } from "@/src/uikit-utils";
 import { css } from "@/styled-system/css";
-import {
-  AnchorButton,
-  Button,
-  COLLATERALS,
-  HFlex,
-  IconBorrow,
-  IconEarn,
-  InfoTooltip,
-  TokenIcon,
-} from "@liquity2/uikit";
+import { AnchorButton, COLLATERALS, HFlex, IconBorrow, IconEarn, InfoTooltip, TokenIcon } from "@liquity2/uikit";
 import * as dn from "dnum";
 import Link from "next/link";
-import { match } from "ts-pattern";
 
 export function HomeScreen() {
-  const { address: accountAddress } = useAccount();
-  const {
-    currentStepIndex,
-    discard,
-    signAndSend,
-    start,
-    flow,
-  } = useTransactionFlow();
-
-  const availableTroveIndex = useFindAvailableTroveIndex(accountAddress);
-  const openedTroveIndex = (availableTroveIndex.data ?? 0) - 1;
-
   return (
     <div
       className={css({
@@ -47,119 +22,6 @@ export function HomeScreen() {
         width: "100%",
       })}
     >
-      <div
-        className={css({
-          display: "flex",
-          flexDirection: "column",
-          gap: 16,
-        })}
-      >
-        <div>
-          <div>
-            next available trove: {match(availableTroveIndex)
-              .with({ status: "idle" }, () => "−")
-              .with({ status: "loading" }, () => "fetching")
-              .with({ status: "error" }, () => "error")
-              .with({ status: "success" }, ({ data }) => `#${data}`)
-              .exhaustive()}
-          </div>
-          <div>flow: {flow?.request.flowId}</div>
-          <div>
-            flow steps:{" "}
-            {flow?.steps && <>[{flow?.steps.map(({ id, txHash }) => txHash ? `${id} (ok)` : id).join(", ")}]</>}
-          </div>
-          <div>
-            current flow step: {currentStepIndex} ({flow?.steps && flow?.steps[currentStepIndex]?.id})
-          </div>
-          <div>
-            flow step error: <pre>{flow?.steps?.[currentStepIndex]?.error}</pre>
-          </div>
-        </div>
-        {accountAddress && availableTroveIndex.status === "success" && (
-          <div
-            className={css({
-              display: "flex",
-              flexDirection: "column",
-              gap: 16,
-            })}
-          >
-            <div
-              className={css({
-                display: "flex",
-                gap: 16,
-              })}
-            >
-              <Button
-                size="mini"
-                label={`openLoanPosition (#${availableTroveIndex.data})`}
-                onClick={() => {
-                  start({
-                    flowId: "openLoanPosition",
-                    owner: accountAddress,
-                    ownerIndex: availableTroveIndex.data,
-                    collAmount: dn.from(25, 18),
-                    boldAmount: dn.from(2800, 18),
-                    upperHint: dn.from(0, 18),
-                    lowerHint: dn.from(0, 18),
-                    annualInterestRate: dn.from(0.05, 18),
-                    maxUpfrontFee: dn.from(100, 18),
-                  });
-                }}
-              />
-              <Button
-                disabled={openedTroveIndex < 0}
-                label={`updateLoanPosition (#${availableTroveIndex.data - 1})`}
-                onClick={() => {
-                  start({
-                    flowId: "updateLoanPosition",
-                    owner: accountAddress,
-                    ownerIndex: availableTroveIndex.data - 1,
-                    collChange: dn.from(1, 18),
-                    boldChange: dn.from(0, 18),
-                    maxUpfrontFee: dn.from(100, 18),
-                  });
-                }}
-                size="mini"
-              />
-              <Button
-                disabled={openedTroveIndex < 0}
-                label={`repayAndCloseLoanPosition (#${availableTroveIndex.data - 1})`}
-                onClick={() => {
-                  start({
-                    flowId: "repayAndCloseLoanPosition",
-                    owner: accountAddress,
-                    ownerIndex: availableTroveIndex.data - 1,
-                  });
-                }}
-                size="mini"
-              />
-            </div>
-            <div
-              className={css({
-                display: "flex",
-                gap: 16,
-              })}
-            >
-              <Button
-                size="mini"
-                label="discard"
-                onClick={discard}
-                disabled={!flow}
-              />
-              <Button
-                size="mini"
-                label="sign & send"
-                onClick={() => {
-                  if (currentStepIndex >= 0) {
-                    signAndSend();
-                  }
-                }}
-                disabled={!flow || currentStepIndex < 0}
-              />
-            </div>
-          </div>
-        )}
-      </div>
       <Positions />
       <div
         className={css({
