14e63a788fdc0c9b9c65902bf29c6a3611a54c12
Pierre Bertet <hi@bpier.re>
2024-07-19 10:07:21 +0100

    DeployLiquity2: only allow the required collateral amount before opening a trove (#247)
    - Separate opening demo troves from sending collateral.
    - Only approve the amount needed to open a trove (collateral + ETH_GAS_COMPENSATION).

--- contracts/src/scripts/DeployLiquity2.s.sol
+++ contracts/src/scripts/DeployLiquity2.s.sol
@@ -6,9 +6,10 @@ import {StdCheats} from "forge-std/StdCheats.sol";
 import "../deployment.sol";
 import {Accounts} from "../test/TestContracts/Accounts.sol";
 import {ERC20Faucet} from "../test/TestContracts/ERC20Faucet.sol";
+import {ETH_GAS_COMPENSATION} from "../Dependencies/Constants.sol";
 
 contract DeployLiquity2Script is Script, StdCheats {
-    struct TroveParams {
+    struct DemoTroveParams {
         uint256 coll;
         uint256 debt;
         uint256 owner;
@@ -28,52 +29,57 @@ contract DeployLiquity2Script is Script, StdCheats {
         vm.stopBroadcast();
 
         if (vm.envOr("OPEN_DEMO_TROVES", false)) {
-            openDemoTroves(contracts.collToken, contracts.borrowerOperations);
+            // Anvil default accounts
+            // TODO: get accounts from env
+            uint256[] memory demoAccounts = new uint256[](8);
+            demoAccounts[0] = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+            demoAccounts[1] = 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d;
+            demoAccounts[2] = 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a;
+            demoAccounts[3] = 0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6;
+            demoAccounts[4] = 0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a;
+            demoAccounts[5] = 0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba;
+            demoAccounts[6] = 0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e;
+            demoAccounts[7] = 0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356;
+
+            DemoTroveParams[] memory demoTroves = new DemoTroveParams[](4);
+            demoTroves[0] = DemoTroveParams({owner: demoAccounts[0], ownerIndex: 0, coll: 25e18, debt: 2800e18});
+            demoTroves[1] = DemoTroveParams({owner: demoAccounts[1], ownerIndex: 0, coll: 37e18, debt: 2400e18});
+            demoTroves[2] = DemoTroveParams({owner: demoAccounts[2], ownerIndex: 0, coll: 30e18, debt: 4000e18});
+            demoTroves[3] = DemoTroveParams({owner: demoAccounts[3], ownerIndex: 0, coll: 65e18, debt: 6000e18});
+
+            tapFaucet(demoAccounts, contracts);
+            openDemoTroves(demoTroves, contracts);
         }
     }
 
-    function openDemoTroves(IERC20 collToken, IBorrowerOperations borrowerOperations) internal {
-        // Anvil accounts
-        // TODO: pass accounts from env
-        uint256[8] memory accounts = [
-            0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80,
-            0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d,
-            0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a,
-            0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6,
-            0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a,
-            0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba,
-            0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e,
-            0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356
-        ];
+    function tapFaucet(uint256[] memory accounts, LiquityContractsDev memory contracts) internal {
+        for (uint256 i = 0; i < accounts.length; i++) {
+            vm.startBroadcast(accounts[i]);
+            ERC20Faucet(address(contracts.collToken)).tap();
+            vm.stopBroadcast();
+        }
+    }
 
-        TroveParams[8] memory troves = [
-            TroveParams({owner: accounts[0], ownerIndex: 0, coll: 25e18, debt: 2800e18}),
-            TroveParams({owner: accounts[1], ownerIndex: 0, coll: 32e18, debt: 2800e18}),
-            TroveParams({owner: accounts[2], ownerIndex: 0, coll: 30e18, debt: 4000e18}),
-            TroveParams({owner: accounts[3], ownerIndex: 0, coll: 65e18, debt: 6000e18}),
-            TroveParams({owner: accounts[4], ownerIndex: 0, coll: 50e18, debt: 5000e18}),
-            TroveParams({owner: accounts[5], ownerIndex: 0, coll: 37e18, debt: 2400e18}),
-            TroveParams({owner: accounts[6], ownerIndex: 0, coll: 37e18, debt: 2800e18}),
-            TroveParams({owner: accounts[7], ownerIndex: 0, coll: 36e18, debt: 2222e18})
-        ];
-
+    function openDemoTroves(DemoTroveParams[] memory troves, LiquityContractsDev memory contracts) internal {
         for (uint256 i = 0; i < troves.length; i++) {
-            vm.startBroadcast(troves[i].owner);
+            DemoTroveParams memory trove = troves[i];
 
-            ERC20Faucet(address(collToken)).tap();
+            vm.startBroadcast(trove.owner);
 
-            // Approve infinite collToken to BorrowerOperations
-            collToken.approve(address(borrowerOperations), type(uint256).max);
+            // Approve collToken to BorrowerOperations
+            IERC20(contracts.collToken).approve(
+                address(contracts.borrowerOperations), trove.coll + ETH_GAS_COMPENSATION
+            );
 
-            borrowerOperations.openTrove(
-                vm.addr(troves[i].owner), // _owner
-                troves[i].ownerIndex, //     _ownerIndex
-                troves[i].coll, //           _collAmount
-                troves[i].debt, //           _boldAmount
-                0, //                        _upperHint
-                0, //                        _lowerHint
-                0.05e18, //                  _annualInterestRate
-                type(uint256).max //         _maxUpfrontFee
+            IBorrowerOperations(contracts.borrowerOperations).openTrove(
+                vm.addr(trove.owner), // _owner
+                trove.ownerIndex, //     _ownerIndex
+                trove.coll, //           _collAmount
+                trove.debt, //           _boldAmount
+                0, //                    _upperHint
+                0, //                    _lowerHint
+                0.05e18, //              _annualInterestRate
+                type(uint256).max //     _maxUpfrontFee
             );
 
             vm.stopBroadcast();
