4dd5129022b1008d039a4d04239a44f490b01c62
ßingen <bingen@liquity.org>
2024-07-28 12:49:55 +0100

    fix: Fix hardhat tests after removal of CheckContract

--- contracts/test/OwnershipTest.js
+++ contracts/test/OwnershipTest.js
@@ -35,24 +35,9 @@ contract("All Liquity functions with onlyOwner modifier", async (accounts) => {
     borrowerOperations = contracts.borrowerOperations;
   });
 
-  const testZeroAddress = async (contract, params, method = "setAddresses", skip = 0) => {
-    await testWrongAddress(contract, params, th.ZERO_ADDRESS, method, skip, "Account cannot be zero address");
-  };
-  const testNonContractAddress = async (contract, params, method = "setAddresses", skip = 0) => {
-    await testWrongAddress(contract, params, bob, method, skip, "Account code size cannot be zero");
-  };
-  const testWrongAddress = async (contract, params, address, method, skip, message) => {
-    for (let i = skip; i < params.length; i++) {
-      const newParams = [...params];
-      newParams[i] = address;
-      await th.assertRevert(contract[method](...newParams, { from: owner }), message);
-    }
-  };
-
   const testDeploymentSetter = async (
     contract,
     numberOfAddresses,
-    checkContract = true,
     twice = true,
     method = "setAddresses",
   ) => {
@@ -61,12 +46,6 @@ contract("All Liquity functions with onlyOwner modifier", async (accounts) => {
 
     // Attempt call from alice
     await th.assertRevert(contract[method](...params, { from: alice }));
-    if (checkContract) {
-      // Attempt to use zero address
-      await testZeroAddress(contract, params, method);
-      // Attempt to use non contract
-      await testNonContractAddress(contract, params, method);
-    }
 
     // Owner can successfully set any address
     const txOwner = await contract[method](...params, { from: owner });
@@ -79,16 +58,16 @@ contract("All Liquity functions with onlyOwner modifier", async (accounts) => {
 
   describe("BoldToken", async (accounts) => {
     it("setBranchAddresses(): reverts when called by non-owner, with wrong addresses", async () => {
-      await testDeploymentSetter(boldToken, 4, false, false, "setBranchAddresses");
+      await testDeploymentSetter(boldToken, 4, false, "setBranchAddresses");
     });
     it("setCollateralRegistry(): reverts when called by non-owner, with wrong address, or twice", async () => {
-      await testDeploymentSetter(boldToken, 1, false, true, "setCollateralRegistry");
+      await testDeploymentSetter(boldToken, 1, true, "setCollateralRegistry");
     });
   });
 
   describe("BorrowerOperations", async (accounts) => {
     it("setAddresses(): reverts when called by non-owner, with wrong addresses, or twice", async () => {
-      await testDeploymentSetter(borrowerOperations, 8, false);
+      await testDeploymentSetter(borrowerOperations, 7);
     });
   });
 
@@ -100,7 +79,7 @@ contract("All Liquity functions with onlyOwner modifier", async (accounts) => {
 
   describe("StabilityPool", async (accounts) => {
     it("setAddresses(): reverts when called by non-owner, with wrong addresses, or twice", async () => {
-      await testDeploymentSetter(stabilityPool, 6, false);
+      await testDeploymentSetter(stabilityPool, 6);
     });
   });
 
@@ -117,11 +96,6 @@ contract("All Liquity functions with onlyOwner modifier", async (accounts) => {
 
       // Attempt call from alice
       await th.assertRevert(sortedTroves.setAddresses(...params, { from: alice }));
-
-      // Attempt to use zero address
-      await testZeroAddress(sortedTroves, params, "setAddresses", 1);
-      // Attempt to use non contract
-      await testNonContractAddress(sortedTroves, params, "setAddresses", 1);
 
       // Owner can successfully set params
       const txOwner = await sortedTroves.setAddresses(...params, { from: owner });
