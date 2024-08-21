a11bb3de19d92f851ce20e7e33540416e6f3b8c6
Pierre Bertet <hi@bpier.re>
2024-08-21 01:48:02 +0100

    DeployLiquity2.s.sol tweaks

--- contracts/src/scripts/DeployLiquity2.s.sol
+++ contracts/src/scripts/DeployLiquity2.s.sol
@@ -26,6 +26,9 @@ import "../CollateralRegistry.sol";
 import "../MockInterestRouter.sol";
 import "../test/TestContracts/PriceFeedTestnet.sol";
 import {WETHTester} from "../test/TestContracts/WETHTester.sol";
+import "forge-std/console.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 contract DeployLiquity2Script is Script, StdCheats {
     bytes32 SALT;
@@ -80,17 +83,16 @@ contract DeployLiquity2Script is Script, StdCheats {
     }
 
     struct DemoTroveParams {
-        uint256 coll;
         uint256 collIndex;
-        uint256 debt;
         uint256 owner;
         uint256 ownerIndex;
+        uint256 coll;
+        uint256 debt;
+        uint256 annualInterestRate;
     }
 
     function run() external {
         SALT = keccak256(abi.encodePacked(block.timestamp));
-
-        bool demo = vm.envOr("OPEN_DEMO_TROVES", false);
 
         if (vm.envBytes("DEPLOYER").length == 20) {
             // address
@@ -109,51 +111,70 @@ contract DeployLiquity2Script is Script, StdCheats {
             1 days //         _tapPeriod
         );
 
-        if (demo) {
-            TroveManagerParams[] memory troveManagerParamsArray = new TroveManagerParams[](2);
-            troveManagerParamsArray[0] = TroveManagerParams(150e16, 110e16, 110e16, 5e16, 10e16);
-            troveManagerParamsArray[1] = TroveManagerParams(150e16, 110e16, 110e16, 5e16, 10e16);
-
-            (LiquityContractsTestnet[] memory contractsArray,,,,) =
-                _deployAndConnectContracts(troveManagerParamsArray, WETH);
-
-            vm.stopBroadcast();
-
-            demoState(contractsArray);
+        if (vm.envOr("OPEN_DEMO_TROVES", false)) {
+            deployWithDemoTroves(WETH);
         } else {
-            TroveManagerParams[] memory troveManagerParamsArray = new TroveManagerParams[](1);
-            troveManagerParamsArray[0] = TroveManagerParams(150e16, 110e16, 110e16, 5e16, 10e16);
-
-            (LiquityContractsTestnet[] memory contractsArray,,,,) =
-                _deployAndConnectContracts(troveManagerParamsArray, WETH);
-            vm.stopBroadcast();
+            deploy(WETH);
         }
     }
 
-    function demoState(LiquityContractsTestnet[] memory contractsArray) internal {
+    function deploy(IWETH WETH) internal {
+        TroveManagerParams[] memory troveManagerParamsArray = new TroveManagerParams[](1);
+        troveManagerParamsArray[0] = TroveManagerParams(150e16, 110e16, 110e16, 5e16, 10e16);
+
+        _deployAndConnectContracts(troveManagerParamsArray, WETH);
+        vm.stopBroadcast();
+    }
+
+    function deployWithDemoTroves(IWETH WETH) internal {
+        TroveManagerParams[] memory troveManagerParamsArray = new TroveManagerParams[](1);
+        troveManagerParamsArray[0] = TroveManagerParams(150e16, 110e16, 110e16, 5e16, 10e16); // WETH
+        // troveManagerParamsArray[1] = TroveManagerParams(150e16, 110e16, 110e16, 5e16, 10e16); // stETH
+
+        (LiquityContractsTestnet[] memory contractsArray,,,,) =
+            _deployAndConnectContracts(troveManagerParamsArray, WETH);
+        vm.stopBroadcast();
+
         // Anvil default accounts
         // TODO: get accounts from env
-        uint256[] memory accounts = new uint256[](8);
-        accounts[0] = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-        accounts[1] = 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d;
-        accounts[2] = 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a;
-        accounts[3] = 0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6;
-        accounts[4] = 0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a;
-        accounts[5] = 0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba;
-        accounts[6] = 0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e;
-        accounts[7] = 0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356;
-
-        DemoTroveParams[] memory troves = new DemoTroveParams[](4);
-        troves[0] = DemoTroveParams({collIndex: 0, owner: accounts[0], ownerIndex: 0, coll: 25e18, debt: 2800e18});
-        troves[1] = DemoTroveParams({collIndex: 0, owner: accounts[1], ownerIndex: 0, coll: 37e18, debt: 2400e18});
-        troves[2] = DemoTroveParams({collIndex: 0, owner: accounts[2], ownerIndex: 0, coll: 30e18, debt: 4000e18});
-        troves[3] = DemoTroveParams({collIndex: 0, owner: accounts[3], ownerIndex: 0, coll: 65e18, debt: 6000e18});
+        uint256[] memory demoAccounts = new uint256[](8);
+        demoAccounts[0] = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+        demoAccounts[1] = 0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d;
+        demoAccounts[2] = 0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a;
+        demoAccounts[3] = 0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6;
+        demoAccounts[4] = 0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a;
+        demoAccounts[5] = 0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba;
+        demoAccounts[6] = 0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e;
+        demoAccounts[7] = 0x4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356;
 
         for (uint256 i = 0; i < contractsArray.length; i++) {
-            tapFaucet(accounts, contractsArray[i]);
+            tapFaucet(demoAccounts, contractsArray[i]);
         }
 
-        openDemoTroves(troves, contractsArray);
+        DemoTroveParams[] memory demoTroves = new DemoTroveParams[](8);
+        demoTroves[0] = DemoTroveParams(0, demoAccounts[0], 0, 25e18, 2800e18, 5.0e16);
+        demoTroves[1] = DemoTroveParams(0, demoAccounts[1], 0, 37e18, 2400e18, 4.7e16);
+        demoTroves[2] = DemoTroveParams(0, demoAccounts[2], 0, 30e18, 4000e18, 3.3e16);
+        demoTroves[3] = DemoTroveParams(0, demoAccounts[3], 0, 65e18, 6000e18, 4.3e16);
+
+        demoTroves[4] = DemoTroveParams(0, demoAccounts[4], 0, 19e18, 2280e18, 5.0e16);
+        demoTroves[5] = DemoTroveParams(0, demoAccounts[5], 0, 48.37e18, 4400e18, 4.7e16);
+        demoTroves[6] = DemoTroveParams(0, demoAccounts[6], 0, 33.92e18, 5500e18, 3.8e16);
+        demoTroves[7] = DemoTroveParams(0, demoAccounts[7], 0, 47.2e18, 6000e18, 4.3e16);
+
+        // demoTroves[8] = DemoTroveParams(1, demoAccounts[0], 0, 21e18, 2200e18, 3.3e16);
+        // demoTroves[9] = DemoTroveParams(1, demoAccounts[1], 1, 11e18, 3000e18, 4.1e16);
+        // demoTroves[10] = DemoTroveParams(1, demoAccounts[2], 1, 8e18, 10000e18, 3.8e16);
+        // demoTroves[11] = DemoTroveParams(1, demoAccounts[3], 1, 12e18, 1200e18, 4.3e16);
+
+        // demoTroves[12] = DemoTroveParams(1, demoAccounts[4], 1, 35e18, 25000e18, 7.0e16);
+        // demoTroves[13] = DemoTroveParams(1, demoAccounts[5], 1, 87e18, 44000e18, 4.4e16);
+        // demoTroves[14] = DemoTroveParams(1, demoAccounts[6], 1, 41e18, 11000e18, 3.3e16);
+        // demoTroves[15] = DemoTroveParams(1, demoAccounts[7], 1, 44e18, 16800e18, 4.4e16);
+
+        for (uint256 i = 0; i < demoTroves.length; i++) {
+            openDemoTrove(demoTroves[i], contractsArray);
+        }
     }
 
     function tapFaucet(uint256[] memory accounts, LiquityContractsTestnet memory contracts) internal {
@@ -161,39 +182,53 @@ contract DeployLiquity2Script is Script, StdCheats {
             vm.startBroadcast(accounts[i]);
             ERC20Faucet(address(contracts.collToken)).tap();
             vm.stopBroadcast();
+
+            // ERC20 token = ERC20(address(contracts.collToken));
+            // console.log(
+            //     "Tap %s (%s) to %s",
+            //     string.concat(formatAmount(token.balanceOf(vm.addr(accounts[i])), 18, 2), " ", token.symbol()),
+            //     address(token),
+            //     vm.addr(accounts[i])
+            // );
         }
     }
 
-    function openDemoTroves(DemoTroveParams[] memory troves, LiquityContractsTestnet[] memory contractsArray)
-        internal
-    {
-        for (uint256 i = 0; i < troves.length; i++) {
-            DemoTroveParams memory trove = troves[i];
-            LiquityContractsTestnet memory contracts = contractsArray[trove.collIndex];
+    function openDemoTrove(DemoTroveParams memory trove, LiquityContractsTestnet[] memory contractsArray) internal {
+        LiquityContractsTestnet memory contracts = contractsArray[trove.collIndex];
 
-            vm.startBroadcast(trove.owner);
+        vm.startBroadcast(trove.owner);
 
-            // Approve collToken to BorrowerOperations
-            IERC20(contracts.collToken).approve(
-                address(contracts.borrowerOperations), trove.coll + ETH_GAS_COMPENSATION
-            );
+        // Approve collToken to BorrowerOperations
+        IERC20(contracts.collToken).approve(address(contracts.borrowerOperations), trove.coll + ETH_GAS_COMPENSATION);
 
-            IBorrowerOperations(contracts.borrowerOperations).openTrove(
-                vm.addr(trove.owner), // _owner
-                trove.ownerIndex, //     _ownerIndex
-                trove.coll, //           _collAmount
-                trove.debt, //           _boldAmount
-                0, //                    _upperHint
-                0, //                    _lowerHint
-                0.05e18, //              _annualInterestRate
-                type(uint256).max, //    _maxUpfrontFee
-                address(0), //           _addManager
-                address(0), //           _removeManager
-                address(0) //           _receiver
-            );
+        IBorrowerOperations(contracts.borrowerOperations).openTrove(
+            vm.addr(trove.owner), // _owner
+            trove.ownerIndex, //     _ownerIndex
+            trove.coll, //           _collAmount
+            trove.debt, //           _boldAmount
+            0, //                    _upperHint
+            0, //                    _lowerHint
+            0.05e18, //              _annualInterestRate
+            type(uint256).max, //    _maxUpfrontFee
+            address(0), //           _addManager
+            address(0), //           _removeManager
+            address(0) //           _receiver
+        );
 
-            vm.stopBroadcast();
-        }
+        vm.stopBroadcast();
+
+        // console.log(
+        //     "\nApproval:\nOwner: %s\nSpender: %s\nAllowance: %s",
+        //     vm.addr(trove.owner),
+        //     address(contracts.borrowerOperations),
+        //     string.concat(
+        //         formatAmount(approved, 18, 2),
+        //         " ",
+        //         ERC20(address(contracts.collToken)).symbol(),
+        //         " ",
+        //         string.concat("(", Strings.toHexString(address(contracts.collToken)), ")")
+        //     )
+        // );
     }
 
     // See: https://solidity-by-example.org/app/create2/
@@ -363,5 +398,24 @@ contract DeployLiquity2Script is Script, StdCheats {
         );
 
         _collateralRegistry.setTroveManager(_branch, contracts.troveManager);
+    }
+
+    function formatAmount(uint256 amount, uint256 decimals, uint256 digits) internal pure returns (string memory) {
+        if (digits > decimals) {
+            digits = decimals;
+        }
+
+        uint256 scaled = amount / (10 ** (decimals - digits));
+        string memory whole = Strings.toString(scaled / (10 ** digits));
+
+        if (digits == 0) {
+            return whole;
+        }
+
+        string memory fractional = Strings.toString(scaled % (10 ** digits));
+        for (uint256 i = bytes(fractional).length; i < digits; i++) {
+            fractional = string.concat("0", fractional);
+        }
+        return string.concat(whole, ".", fractional);
     }
 }
