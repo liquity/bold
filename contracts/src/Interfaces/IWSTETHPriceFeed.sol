81a1a2aae995c21e9c4f7fc2e6b4bb4f7237798b
RickGriff <rick@liquity.org>
2024-08-29 16:52:30 +0100

    Add ETH-USD fallback logic and remove OSETH and ETHX contracts

--- contracts/src/Interfaces/IWSTETHPriceFeed.sol
+++ contracts/src/Interfaces/IWSTETHPriceFeed.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
-import "./IPriceFeed.sol";
+import "./IMainnetPriceFeed.sol";
 import "../Dependencies/AggregatorV3Interface.sol";
 
 pragma solidity ^0.8.0;
 
-interface IWSTETHPriceFeed is IPriceFeed {
+interface IWSTETHPriceFeed is IMainnetPriceFeed {
     function stEthUsdOracle() external view returns (AggregatorV3Interface, uint256, uint8);
 }
