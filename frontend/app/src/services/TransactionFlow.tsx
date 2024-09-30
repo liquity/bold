76375102d078f672a9b5d5c7dedf2732824b7d85
Pierre Bertet <hi@bpier.re>
2024-09-30 10:51:16 +0100

    Add the earnDeposit transaction flow

--- frontend/app/src/services/TransactionFlow.tsx
+++ frontend/app/src/services/TransactionFlow.tsx
@@ -12,6 +12,7 @@
 
 import type { Contracts } from "@/src/contracts";
 import type { Request as CloseLoanPositionRequest } from "@/src/tx-flows/closeLoanPosition";
+import type { Request as EarnDepositRequest } from "@/src/tx-flows/earnDeposit";
 import type { Request as OpenLoanPositionRequest } from "@/src/tx-flows/openLoanPosition";
 import type { Request as UpdateLoanInterestRateRequest } from "@/src/tx-flows/updateLoanInterestRate";
 import type { Request as UpdateLoanPositionRequest } from "@/src/tx-flows/updateLoanPosition";
@@ -24,6 +25,7 @@ import { useContracts } from "@/src/contracts";
 import { jsonParseWithDnum, jsonStringifyWithDnum } from "@/src/dnum-utils";
 import { useAccount, useWagmiConfig } from "@/src/services/Ethereum";
 import { closeLoanPosition } from "@/src/tx-flows/closeLoanPosition";
+import { earnDeposit } from "@/src/tx-flows/earnDeposit";
 import { openLoanPosition } from "@/src/tx-flows/openLoanPosition";
 import { updateLoanInterestRate } from "@/src/tx-flows/updateLoanInterestRate";
 import { updateLoanPosition } from "@/src/tx-flows/updateLoanPosition";
@@ -37,10 +39,11 @@ import { useTransactionReceipt, useWriteContract } from "wagmi";
 const TRANSACTION_FLOW_KEY = `${LOCAL_STORAGE_PREFIX}transaction_flow`;
 
 export type FlowRequest =
+  | CloseLoanPositionRequest
+  | EarnDepositRequest
   | OpenLoanPositionRequest
-  | UpdateLoanPositionRequest
   | UpdateLoanInterestRateRequest
-  | CloseLoanPositionRequest;
+  | UpdateLoanPositionRequest;
 
 const flowDeclarations: {
   [K in FlowIdFromFlowRequest<FlowRequest>]: FlowDeclaration<
@@ -49,6 +52,7 @@ const flowDeclarations: {
   >;
 } = {
   closeLoanPosition,
+  earnDeposit,
   openLoanPosition,
   updateLoanInterestRate,
   updateLoanPosition,
