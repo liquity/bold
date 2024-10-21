4dc03d98c6c9711ceb88aed5465a36cc86927fdb
ßingen <bingen@liquity.org>
2024-10-21 22:10:01 +0100

    chore: Remove floating pragmas

--- contracts/src/Dependencies/AddRemoveManagers.sol
+++ contracts/src/Dependencies/AddRemoveManagers.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity ^0.8.18;
+pragma solidity 0.8.18;
 
 import "../Interfaces/IAddRemoveManagers.sol";
 import "../Interfaces/IAddressesRegistry.sol";
