f5a8ec39394c3af615774736b1a961fb9d71c85f
ßingen <bingen@liquity.org>
2024-10-24 17:21:00 +0100

    fix: Remove unnecessary and inconsistent result form swapFromBold in Zappers

--- contracts/src/Zappers/Interfaces/IExchange.sol
+++ contracts/src/Zappers/Interfaces/IExchange.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 interface IExchange {
-    function swapFromBold(uint256 _boldAmount, uint256 _minCollAmount) external returns (uint256);
+    function swapFromBold(uint256 _boldAmount, uint256 _minCollAmount) external;
 
     function swapToBold(uint256 _collAmount, uint256 _minBoldAmount) external returns (uint256);
 }
