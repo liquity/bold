4a70bef3c2ee36363be2c4f92854640729d13c05
Pierre Bertet <hi@bpier.re>
2024-11-07 15:04:07 +0000

    Point the contracts addresses to the new Sepolia deployment
    Also removes .graphclient from the repository to generate it instead.

--- subgraph/addresses.ts
+++ subgraph/addresses.ts
@@ -1,8 +1,8 @@
 // Sepolia
 export let leverageZappers = [
-  "0x709b1d298f202e0b0a72fd92769d7f81e9dc5911",
-  "0x75e7f939aec5ca455e4e32cd4b6567b70323bd4a",
-  "0x78b88fc6043810c4d3f0e6af84310620f91cf721",
+  "0xba0c2d6c6c11af89d970bba7c98d365dcb4c3e6f",
+  "0x33df5aa0810df1f9c44b9d9b449c33796f34237c",
+  "0xa5abae953e0a2fd6cd6ed64254b4c2ac643dca8c",
 ];
 export let governanceDeploymentInitiatives = [
   "0x510f6888fd475A7b81C3652696eb807374d65400", // UniV4Donations
