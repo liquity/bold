8f4f90f769ebd8031c3dc3526b183b19b0498f0c
Pierre Bertet <hi@bpier.re>
2024-11-25 10:24:11 +0000

    Loan screen interest rate update: only allow to submit if the rate has changed

--- frontend/app/src/screens/LoanScreen/PanelUpdateRate.tsx
+++ frontend/app/src/screens/LoanScreen/PanelUpdateRate.tsx
@@ -79,7 +79,8 @@ export function PanelUpdateRate({
     && debt.parsed
     && dn.gt(debt.parsed, 0)
     && interestRate
-    && dn.gt(interestRate, 0);
+    && dn.gt(interestRate, 0)
+    && !dn.eq(interestRate, loan.interestRate);
 
   return (
     <>
