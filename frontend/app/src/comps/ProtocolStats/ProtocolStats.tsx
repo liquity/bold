006693706ab4eb369330f18a2fd96b61b80c6221
Pierre Bertet <hi@bpier.re>
2024-11-26 19:01:54 +0000

    Add a custom ID on the account button

--- frontend/app/src/comps/ProtocolStats/ProtocolStats.tsx
+++ frontend/app/src/comps/ProtocolStats/ProtocolStats.tsx
@@ -85,6 +85,7 @@ export function ProtocolStats() {
           })}
           {account.address && (
             <AnchorTextButton
+              id="footer-account-button"
               href={`/account?address=${account.address}`}
               label={
                 <HFlex gap={4} alignItems="center">
