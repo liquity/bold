10f2139f5654a1bb297efb7ac38f89f19ae50947
Daniel Simon <60874270+danielattilasimon@users.noreply.github.com>
2024-12-22 12:52:08 +0700

    Revert "Add state needed for calculating voting power to subgraph"

---
+++ subgraph/addresses.ts
@@ -0,0 +1,7 @@
+// Sepolia
+export let governanceDeploymentInitiatives = [
+  "0x510f6888fd475A7b81C3652696eb807374d65400", // UniV4Donations
+];
+
+// Mainnet
+// export let governanceDeploymentInitiatives = [];
