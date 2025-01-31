97a971a04a92c6cf3f2b9e3ff5258b638b9d1583
johnnyonline <johnnyonline.eth@gmail.com>
2025-01-31 16:58:25 -0700

    fix: https://github.com/yAudit/USA.d-report/issues/4

--- contracts/src/PriceFeeds/USA.D/TbtcOracle.sol
+++ contracts/src/PriceFeeds/USA.D/TbtcOracle.sol
@@ -7,9 +7,9 @@ contract TbtcOracle is BaseOracle {
 
     AggregatorV3Interface public immutable FALLBACK_ORACLE;
 
-    uint256 private constant _CL_TBTC_BTC_HEARTBEAT = _24_HOURS;
+    uint256 private constant _CL_TBTC_USD_HEARTBEAT = _24_HOURS;
 
-    AggregatorV3Interface public constant CL_TBTC_BTC_PRICE_FEED = AggregatorV3Interface(0x8350b7De6a6a2C1368E7D4Bd968190e13E354297);
+    AggregatorV3Interface public constant CL_TBTC_USD_PRICE_FEED = AggregatorV3Interface(0x8350b7De6a6a2C1368E7D4Bd968190e13E354297);
 
     constructor(address _fallback) BaseOracle("tBTC / USD") {
         FALLBACK_ORACLE = AggregatorV3Interface(_fallback);
@@ -21,8 +21,8 @@ contract TbtcOracle is BaseOracle {
         override
         returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
     {
-        (roundId, answer, startedAt, updatedAt, answeredInRound) = CL_TBTC_BTC_PRICE_FEED.latestRoundData();
-        if (_isStale(answer, updatedAt, _CL_TBTC_BTC_HEARTBEAT) && address(FALLBACK_ORACLE) != address(0)) {
+        (roundId, answer, startedAt, updatedAt, answeredInRound) = CL_TBTC_USD_PRICE_FEED.latestRoundData();
+        if (_isStale(answer, updatedAt, _CL_TBTC_USD_HEARTBEAT) && address(FALLBACK_ORACLE) != address(0)) {
             (roundId, answer, startedAt, updatedAt, answeredInRound) = FALLBACK_ORACLE.latestRoundData();
         }
         return (roundId, answer, startedAt, updatedAt, answeredInRound);
