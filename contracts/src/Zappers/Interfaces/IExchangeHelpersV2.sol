628c4a283db278934f0e4f47a62e6e906eab521c
Daniel Simon <daniel.attila.simon@gmail.com>
2025-02-08 14:34:17 +0700

    test: exchange helpers V2

--- contracts/src/Zappers/Interfaces/IExchangeHelpersV2.sol
+++ contracts/src/Zappers/Interfaces/IExchangeHelpersV2.sol
@@ -2,9 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-
 interface IExchangeHelpersV2 {
-    function getDy(uint256 _dx, bool _collToBold, IERC20 _collToken) external returns (uint256 dy);
-    function getDx(uint256 _dy, bool _collToBold, IERC20 _collToken) external returns (uint256 dx);
+    function getDy(uint256 _dx, bool _collToBold, address _collToken) external returns (uint256 dy);
+    function getDx(uint256 _dy, bool _collToBold, address _collToken) external returns (uint256 dx);
 }
--- contracts/src/Zappers/Modules/Exchanges/HybridCurveUniV3ExchangeHelpersV2.sol
+++ contracts/src/Zappers/Modules/Exchanges/HybridCurveUniV3ExchangeHelpersV2.sol
@@ -2,20 +2,18 @@
 
 pragma solidity ^0.8.18;
 
-import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
-import {IWETH} from "../../../Interfaces/IWETH.sol";
 import {ICurveStableswapNGPool} from "./Curve/ICurveStableswapNGPool.sol";
 import {IQuoterV2} from "./UniswapV3/IQuoterV2.sol";
 import {IExchangeHelpersV2} from "../../Interfaces/IExchangeHelpersV2.sol";
 
 contract HybridCurveUniV3ExchangeHelpersV2 is IExchangeHelpersV2 {
-    IERC20 public immutable USDC;
-    IWETH public immutable WETH;
+    address public immutable USDC;
+    address public immutable WETH;
 
     // Curve
     ICurveStableswapNGPool public immutable curvePool;
-    uint128 public immutable USDC_INDEX;
-    uint128 public immutable BOLD_TOKEN_INDEX;
+    int128 public immutable USDC_INDEX;
+    int128 public immutable BOLD_TOKEN_INDEX;
 
     // Uniswap
     uint24 public immutable feeUsdcWeth;
@@ -23,12 +21,12 @@ contract HybridCurveUniV3ExchangeHelpersV2 is IExchangeHelpersV2 {
     IQuoterV2 public immutable uniV3Quoter;
 
     constructor(
-        IERC20 _usdc,
-        IWETH _weth,
+        address _usdc,
+        address _weth,
         // Curve
         ICurveStableswapNGPool _curvePool,
-        uint128 _usdcIndex,
-        uint128 _boldIndex,
+        int128 _usdcIndex,
+        int128 _boldIndex,
         // UniV3
         uint24 _feeUsdcWeth,
         uint24 _feeWethColl,
@@ -48,11 +46,11 @@ contract HybridCurveUniV3ExchangeHelpersV2 is IExchangeHelpersV2 {
         uniV3Quoter = _uniV3Quoter;
     }
 
-    function getDy(uint256 _dx, bool _collToBold, IERC20 _collToken) external returns (uint256 dy) {
+    function getDy(uint256 _dx, bool _collToBold, address _collToken) external returns (uint256 dy) {
         if (_collToBold) {
             // (Coll ->) WETH -> USDC?
             bytes memory path;
-            if (address(WETH) == address(_collToken)) {
+            if (WETH == _collToken) {
                 path = abi.encodePacked(WETH, feeUsdcWeth, USDC);
             } else {
                 path = abi.encodePacked(_collToken, feeWethColl, WETH, feeUsdcWeth, USDC);
@@ -61,14 +59,14 @@ contract HybridCurveUniV3ExchangeHelpersV2 is IExchangeHelpersV2 {
             (uint256 uniDy,,,) = uniV3Quoter.quoteExactInput(path, _dx);
 
             // USDC -> BOLD?
-            dy = curvePool.get_dy(int128(USDC_INDEX), int128(BOLD_TOKEN_INDEX), uniDy);
+            dy = curvePool.get_dy(USDC_INDEX, BOLD_TOKEN_INDEX, uniDy);
         } else {
             // BOLD -> USDC?
-            uint256 curveDy = curvePool.get_dy(int128(BOLD_TOKEN_INDEX), int128(USDC_INDEX), _dx);
+            uint256 curveDy = curvePool.get_dy(BOLD_TOKEN_INDEX, USDC_INDEX, _dx);
 
             // USDC -> WETH (-> Coll)?
             bytes memory path;
-            if (address(WETH) == address(_collToken)) {
+            if (WETH == _collToken) {
                 path = abi.encodePacked(USDC, feeUsdcWeth, WETH);
             } else {
                 path = abi.encodePacked(USDC, feeUsdcWeth, WETH, feeWethColl, _collToken);
@@ -78,15 +76,15 @@ contract HybridCurveUniV3ExchangeHelpersV2 is IExchangeHelpersV2 {
         }
     }
 
-    function getDx(uint256 _dy, bool _collToBold, IERC20 _collToken) external returns (uint256 dx) {
+    function getDx(uint256 _dy, bool _collToBold, address _collToken) external returns (uint256 dx) {
         if (_collToBold) {
             // USDC? -> BOLD
-            uint256 curveDx = curvePool.get_dx(int128(USDC_INDEX), int128(BOLD_TOKEN_INDEX), _dy);
+            uint256 curveDx = curvePool.get_dx(USDC_INDEX, BOLD_TOKEN_INDEX, _dy);
 
             // Uniswap expects path to be reversed when quoting exact output
             // USDC <- WETH (<- Coll)?
             bytes memory path;
-            if (address(WETH) == address(_collToken)) {
+            if (WETH == _collToken) {
                 path = abi.encodePacked(USDC, feeUsdcWeth, WETH);
             } else {
                 path = abi.encodePacked(USDC, feeUsdcWeth, WETH, feeWethColl, _collToken);
@@ -97,7 +95,7 @@ contract HybridCurveUniV3ExchangeHelpersV2 is IExchangeHelpersV2 {
             // Uniswap expects path to be reversed when quoting exact output
             // (Coll <-) WETH <- USDC?
             bytes memory path;
-            if (address(WETH) == address(_collToken)) {
+            if (WETH == _collToken) {
                 path = abi.encodePacked(WETH, feeUsdcWeth, USDC);
             } else {
                 path = abi.encodePacked(_collToken, feeWethColl, WETH, feeUsdcWeth, USDC);
@@ -106,7 +104,7 @@ contract HybridCurveUniV3ExchangeHelpersV2 is IExchangeHelpersV2 {
             (uint256 uniDx,,,) = uniV3Quoter.quoteExactOutput(path, _dy);
 
             // BOLD? -> USDC
-            dx = curvePool.get_dx(int128(BOLD_TOKEN_INDEX), int128(USDC_INDEX), uniDx);
+            dx = curvePool.get_dx(BOLD_TOKEN_INDEX, USDC_INDEX, uniDx);
         }
     }
 }
