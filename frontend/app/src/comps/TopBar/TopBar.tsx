68232ac882a73e0355e0af794d038c1756aa8dcc
geovgy <gbh218@gmail.com>
2025-02-21 15:33:22 -0500

    Replace liquity icons with snails

--- frontend/app/src/comps/TopBar/TopBar.tsx
+++ frontend/app/src/comps/TopBar/TopBar.tsx
@@ -19,11 +19,11 @@ import { AccountButton } from "./AccountButton";
 import { Menu } from "./Menu";
 
 const menuItems: ComponentProps<typeof Menu>["menuItems"] = [
-  [content.menu.dashboard, "/", IconDashboard],
-  [content.menu.borrow, "/borrow", IconBorrow],
-  [content.menu.multiply, "/multiply", IconLeverage],
-  [content.menu.earn, "/earn", IconEarn],
-  [content.menu.buy, "/buy", IconStake],
+  [content.menu.dashboard, "/", IconDashboard, "dashboard"],
+  [content.menu.borrow, "/borrow", IconBorrow, "borrow"],
+  [content.menu.multiply, "/multiply", IconLeverage, "multiply"],
+  [content.menu.earn, "/earn", IconEarn, "earn"],
+  [content.menu.buy, "/buy", IconStake, "buy"],
 ];
 
 export function TopBar() {
