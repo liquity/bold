eb9d082a4625f3ede339fc96f347a0427557c1d9
Pierre Bertet <hi@bpier.re>
2025-03-07 18:34:45 +0000

    Tweak loading state

--- frontend/app/src/screens/LegacyScreen/LegacyScreen.tsx
+++ frontend/app/src/screens/LegacyScreen/LegacyScreen.tsx
@@ -11,6 +11,7 @@ import { useTransactionFlow } from "@/src/services/TransactionFlow";
 import { useAccount } from "@/src/wagmi-utils";
 import { css } from "@/styled-system/css";
 import { Button, IconBorrow, IconEarn, IconStake, TokenIcon } from "@liquity2/uikit";
+import { a, useTransition } from "@react-spring/web";
 import * as dn from "dnum";
 import { notFound } from "next/navigation";
 
@@ -30,56 +31,97 @@ export function LegacyScreen() {
   const account = useAccount();
   const legacyPositions = useLegacyPositions(account.address ?? null);
 
+  const positionsTransition = useTransition(account.isConnected, {
+    from: {
+      opacity: 0,
+      transform: `
+        scale3d(0.97, 0.97, 1)
+        translate3d(0, 8px, 0)
+      `,
+    },
+    enter: {
+      opacity: 1,
+      transform: `
+        scale3d(1, 1, 1)
+        translate3d(0, 0px, 0)
+      `,
+    },
+    leave: {
+      display: "none",
+      immediate: true,
+    },
+    config: {
+      mass: 1,
+      tension: 2800,
+      friction: 120,
+    },
+  });
+
   return (
     <Screen
       heading={{
         title: "Liquity V2-Legacy Positions",
       }}
     >
-      {account.isConnected
-        ? (
-          <>
-            <div
-              className={css({
-                display: "flex",
-                flexDirection: "column",
-                gap: 32,
-                padding: 16,
-                textAlign: "center",
-                textWrap: "balance",
-                color: "content",
-                background: "infoSurface",
-                border: "1px solid token(colors.infoSurfaceBorder)",
-                borderRadius: 8,
-              })}
-            >
-              {!account.isConnected
-                ? "Connect your wallet to view your legacy positions."
-                : legacyPositions.isFetching
-                ? "Loading your legacy positions…"
-                : legacyPositions.data?.hasAnyPosition
-                ? `
-                    You have active positions in Liquity V2-Legacy.
-                    These positions are not compatible with Liquity V2.
-                    You can withdraw these positions from here at any time.
-                  `
-                : "You do not have any active positions in Liquity V2-Legacy."}
-            </div>
-            <div
+      {positionsTransition((style, show) => (
+        show
+          ? (
+            <a.div
               className={css({
                 display: "flex",
                 flexDirection: "column",
                 gap: 48,
-                width: 534,
+                willChange: "transform, opacity",
               })}
+              style={style}
             >
-              <EarnPositionsTable />
-              <LoanPositionsTable />
-              <StakingPositionsTable />
-            </div>
-          </>
-        )
-        : <ConnectWarningBox />}
+              <div
+                className={css({
+                  display: "flex",
+                  flexDirection: "column",
+                  padding: 16,
+                  textAlign: "center",
+                  textWrap: "balance",
+                  color: "content",
+                  background: "infoSurface",
+                  border: "1px solid token(colors.infoSurfaceBorder)",
+                  borderRadius: 8,
+                })}
+              >
+                {legacyPositions.isLoading
+                  ? `Fetching your legacy positions…`
+                  : legacyPositions.data?.hasAnyPosition
+                  ? `You have active positions in Liquity V2-Legacy. These positions are not compatible with Liquity V2.
+                         You can withdraw these positions from here at any time.`
+                  : `You do not have any active positions in Liquity V2-Legacy.`}
+              </div>
+              {legacyPositions.isSuccess && (
+                <div
+                  className={css({
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 48,
+                    width: 534,
+                  })}
+                >
+                  <EarnPositionsTable />
+                  <LoanPositionsTable />
+                  <StakingPositionsTable />
+                </div>
+              )}
+            </a.div>
+          )
+          : (
+            <a.div
+              className={css({
+                willChange: "transform, opacity",
+              })}
+              style={style}
+            >
+              <ConnectWarningBox />
+            </a.div>
+          )
+      ))}
     </Screen>
   );
 }
