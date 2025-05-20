dba0ebec6fab5b3c2a98d45143492015c92e6f6b
Esteban Suárez <estebansuarez.blog@gmail.com>
2025-05-20 14:30:30 -0300

    Update AboutButton.tsx

--- frontend/app/src/comps/AppLayout/AboutButton.tsx
+++ frontend/app/src/comps/AppLayout/AboutButton.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { useAbout } from "@/src/comps/About/About";
 import { css } from "@/styled-system/css";
 import { TextButton } from "@liquity2/uikit";
+import { LinkTextButton } from "@/src/comps/LinkTextButton/LinkTextButton";
+import { HFlex } from "@liquity2/uikit";
 
 export function AboutButton({
   onClick,
@@ -8,20 +12,29 @@ export function AboutButton({
   onClick?: () => void;
 }) {
   const about = useAbout();
+
   return (
-    <TextButton
-      label={about.fullVersion}
-      title={`About Liquity V2 App ${about.fullVersion}`}
-      onClick={() => {
-        about.openModal();
-        onClick?.();
-      }}
-      className={css({
-        color: "dimmed",
-      })}
-      style={{
-        fontSize: 12,
-      }}
-    />
+    <HFlex
+      alignItems="center"
+      gap={8}
+      className={css({ color: "dimmed", fontSize: 12 })}
+    >
+      {/* version / “About” button */}
+      <TextButton
+        label={about.fullVersion}
+        title={`About Liquity V2 App ${about.fullVersion}`}
+        onClick={() => {
+          about.openModal();
+          onClick?.();
+        }}
+      />
+
+      {/* your “frontend run by…” link */}
+      <LinkTextButton
+        external
+        href="https://twitter.com/estebansuarez"
+        label="Frontend operated by @estebansuarez"
+      />
+    </HFlex>
   );
 }
