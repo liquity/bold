d6008d4889c1726c6e4ec069ec79bde0908efe0e
johnnyonline <johnnyonline.eth@gmail.com>
2025-05-21 21:24:20 -0300

    fix: https://github.com/electisec/usdaf-v2-report/issues/1#issue-3077469165

--- contracts/src/PriceFeeds/USDaf/SusdeOracle.sol
+++ contracts/src/PriceFeeds/USDaf/SusdeOracle.sol
@@ -5,8 +5,6 @@ import {AggregatorV3Interface, BaseOracle} from "./BaseOracle.sol";
 
 contract SusdeOracle is BaseOracle {
 
-    AggregatorV3Interface public immutable FALLBACK_ORACLE;
-
     uint256 private constant _CL_SUSDE_USD_HEARTBEAT = _24_HOURS;
 
     AggregatorV3Interface public constant CL_SUSDE_USD_PRICE_FEED = AggregatorV3Interface(0xFF3BC18cCBd5999CE63E788A1c250a88626aD099);
@@ -20,9 +18,6 @@ contract SusdeOracle is BaseOracle {
         returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
     {
         (roundId, answer, startedAt, updatedAt, answeredInRound) = CL_SUSDE_USD_PRICE_FEED.latestRoundData();
-        if (_isStale(answer, updatedAt, _CL_SUSDE_USD_HEARTBEAT) && address(FALLBACK_ORACLE) != address(0)) {
-            (roundId, answer, startedAt, updatedAt, answeredInRound) = FALLBACK_ORACLE.latestRoundData();
-        }
         return (roundId, answer, startedAt, updatedAt, answeredInRound);
     }
 }
