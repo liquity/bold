184d34359fbb321e5d84c96cc08f7c0cf9534ff6
Pierre Bertet <hi@bpier.re>
2025-05-22 17:57:41 +0100

    dnum18(): accept null / undefined

--- frontend/app/src/dnum-utils.ts
+++ frontend/app/src/dnum-utils.ts
@@ -2,8 +2,11 @@ import type { Dnum, Numberish } from "dnum";
 
 import * as dn from "dnum";
 
-export function dnum18(value: string | bigint | number): Dnum {
-  return [BigInt(value), 18];
+export function dnum18(value: null | undefined): null;
+export function dnum18(value: string | bigint | number): Dnum;
+export function dnum18(value: string | bigint | number | null | undefined): Dnum | null;
+export function dnum18(value: string | bigint | number | null | undefined): Dnum | null {
+  return value === undefined || value === null ? null : [BigInt(value), 18];
 }
 
 export function dnumOrNull(value: Numberish | null | undefined, decimals: number): Dnum | null {
