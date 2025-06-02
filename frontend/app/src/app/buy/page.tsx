54e9f7601d17ede46a6522112cfe791c031489d3
RedVeil <leon.niesler@gmail.com>
2025-06-02 12:37:34 +0200

    coming soon for buy page

--- frontend/app/src/app/buy/page.tsx
+++ frontend/app/src/app/buy/page.tsx
@@ -1,5 +1,10 @@
 import { BuyScreen } from "@/src/screens/BuyScreen/BuyScreen";
 
 export default function Page() {
-  return <BuyScreen />;
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <p className="text-2xl font-bold">Coming Soon</p>
+    </div>
+  );
+  // return <BuyScreen />;
 }
