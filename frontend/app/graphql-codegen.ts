5236a8121c3bc51aafcefb83fccc923da8a50497
RedVeil <leon.niesler@gmail.com>
2025-06-12 12:22:46 +0200

    findSubgraph

--- frontend/app/graphql-codegen.ts
+++ frontend/app/graphql-codegen.ts
@@ -23,8 +23,8 @@ function findSubgraphUrl(envFile: string, key: string) {
   }
 }
 
-const subgraphUrl = "https://gateway.thegraph.com/api/subgraphs/id/9KA2FiTvU1k4qMTzSPLwFTwwqDu2fejksQXXtbG45MiM" //findSubgraphUrl(".env.local", "NEXT_PUBLIC_SUBGRAPH_URL") ?? findSubgraphUrl(".env", "NEXT_PUBLIC_SUBGRAPH_URL");
-const subgraphApiKey = process.env.NEXT_PUBLIC_SUBGRAPH_API_KEY //findSubgraphUrl(".env.local", "NEXT_PUBLIC_SUBGRAPH_API_KEY") ?? findSubgraphUrl(".env", "NEXT_PUBLIC_SUBGRAPH_API_KEY");
+const subgraphUrl = findSubgraphUrl(".env.local", "NEXT_PUBLIC_SUBGRAPH_URL") ?? findSubgraphUrl(".env", "NEXT_PUBLIC_SUBGRAPH_URL");
+const subgraphApiKey = findSubgraphUrl(".env.local", "NEXT_PUBLIC_SUBGRAPH_API_KEY") ?? findSubgraphUrl(".env", "NEXT_PUBLIC_SUBGRAPH_API_KEY");
 
 if (!subgraphUrl) {
   throw new Error(
