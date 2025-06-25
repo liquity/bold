c271acf964979630161a893ffbb1d1e222b8d8fa
Pierre Bertet <hi@bpier.re>
2025-06-25 18:38:27 +0100

    Fix build

--- frontend/app/vite.config.ts
+++ frontend/app/vite.config.ts
@@ -1,9 +1,14 @@
+import type { ViteUserConfig } from "vitest/config";
+
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+  ] as NonNullable<ViteUserConfig["plugins"]>,
   test: {
     coverage: {
       include: [
