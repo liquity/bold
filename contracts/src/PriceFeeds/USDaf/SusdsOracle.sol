81209aea1b9c93e048a38fa1811b454feb8b086a
johnnyonline <johnnyonline.eth@gmail.com>
2025-07-11 13:54:02 -0300

    fix: https://github.com/electisec/usdaf-v2-pr-report/issues/1#issue-3211592773

--- contracts/src/PriceFeeds/USDaf/SusdsOracle.sol
+++ contracts/src/PriceFeeds/USDaf/SusdsOracle.sol
@@ -5,17 +5,17 @@ import {ERC4626Oracle} from "./ERC4626Oracle.sol";
 
 contract SusdsOracle is ERC4626Oracle {
 
-    uint256 private constant _CL_DAI_USD_HEARTBEAT = _1_HOUR;
+    uint256 private constant _CL_USDS_USD_HEARTBEAT = _24_HOURS;
 
-    address private constant _CL_DAI_USD_PRICE_FEED = 0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9;
+    address private constant _CL_USDS_USD_PRICE_FEED = 0xfF30586cD0F29eD462364C7e81375FC0C71219b1;
     address private constant _SUSDS = 0xa3931d71877C0E7a3148CB7Eb4463524FEc27fbD;
 
     constructor()
         ERC4626Oracle(
             "sUSDS / USD", // description
-            _CL_DAI_USD_HEARTBEAT, // heartbeat
+            _CL_USDS_USD_HEARTBEAT, // heartbeat
             _SUSDS, // token
-            _CL_DAI_USD_PRICE_FEED, // primary
+            _CL_USDS_USD_PRICE_FEED, // primary
             address(0) // fallback
         ) {}
 }
