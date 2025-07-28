7295f6cedd0d3818462e905de7bf0f52580242e4
Daniel Simon <daniel.attila.simon@gmail.com>
2025-07-28 16:58:13 +0700

    fix: move redemption link & remove logo

--- frontend/app/src/comps/AppLayout/BottomBar.tsx
+++ frontend/app/src/comps/AppLayout/BottomBar.tsx
@@ -136,6 +136,28 @@ export function BottomBar() {
                 )}
               </span>
             </div>
+            <LinkTextButton
+              id="footer-redeem-button"
+              href="/redeem"
+              label={
+                <div
+                  className={css({
+                    display: "flex",
+                    alignItems: "center",
+                    gap: 4,
+                    whiteSpace: "nowrap",
+                  })}
+                >
+                  Redeem BOLD
+                </div>
+              }
+              className={css({
+                color: "content",
+                borderRadius: 4,
+                _focusVisible: { outline: "2px solid token(colors.focused)" },
+                _active: { translate: "0 1px" },
+              })}
+            />
           </div>
           <div
             className={css({
@@ -181,28 +203,6 @@ export function BottomBar() {
                 })}
               />
             )}
-            <LinkTextButton
-              id="footer-redeem-button"
-              href="/redeem"
-              label={
-                <div
-                  className={css({
-                    display: "flex",
-                    alignItems: "center",
-                    gap: 4,
-                    whiteSpace: "nowrap",
-                  })}
-                >
-                  <TokenIcon size={16} symbol="BOLD" /> Redeem BOLD
-                </div>
-              }
-              className={css({
-                color: "content",
-                borderRadius: 4,
-                _focusVisible: { outline: "2px solid token(colors.focused)" },
-                _active: { translate: "0 1px" },
-              })}
-            />
           </div>
         </div>
       </div>
