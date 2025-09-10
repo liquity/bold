d6b381173ce3e99afc2f70432ab8d3b60f997184
Bayological <6872903+bayological@users.noreply.github.com>
2025-09-10 12:27:14 -0500

    refactor: watch it burn 🧑‍🚒

--- contracts/src/Dependencies/Constants.sol
+++ contracts/src/Dependencies/Constants.sol
@@ -9,79 +9,11 @@ uint256 constant DECIMAL_PRECISION = 1e18;
 uint256 constant _100pct = DECIMAL_PRECISION;
 uint256 constant _1pct = DECIMAL_PRECISION / 100;
 
-// Amount of ETH to be locked in gas pool on opening troves
-uint256 constant ETH_GAS_COMPENSATION = 0.0375 ether;
-
-// Liquidation
-uint256 constant MIN_LIQUIDATION_PENALTY_SP = 5e16; // 5%
-uint256 constant MAX_LIQUIDATION_PENALTY_REDISTRIBUTION = 20e16; // 20%
-
-// Collateral branch parameters (SETH = staked ETH, i.e. wstETH / rETH)
-uint256 constant CCR_WETH = 150 * _1pct;
-uint256 constant CCR_SETH = 160 * _1pct;
-
-uint256 constant MCR_WETH = 110 * _1pct;
-uint256 constant MCR_SETH = 120 * _1pct;
-
-uint256 constant SCR_WETH = 110 * _1pct;
-uint256 constant SCR_SETH = 120 * _1pct;
-
-// Batch CR buffer (same for all branches for now)
-// On top of MCR to join a batch, or adjust inside a batch
-uint256 constant BCR_ALL = 10 * _1pct;
-
-uint256 constant LIQUIDATION_PENALTY_SP_WETH = 5 * _1pct;
-uint256 constant LIQUIDATION_PENALTY_SP_SETH = 5 * _1pct;
-
-uint256 constant LIQUIDATION_PENALTY_REDISTRIBUTION_WETH = 10 * _1pct;
-uint256 constant LIQUIDATION_PENALTY_REDISTRIBUTION_SETH = 20 * _1pct;
-
-// Fraction of collateral awarded to liquidator
-uint256 constant COLL_GAS_COMPENSATION_DIVISOR = 200; // dividing by 200 yields 0.5%
-uint256 constant COLL_GAS_COMPENSATION_CAP = 2 ether; // Max coll gas compensation capped at 2 ETH
-
-// Minimum amount of net Bold debt a trove must have
-uint256 constant MIN_DEBT = 2000e18;
-
-uint256 constant MIN_ANNUAL_INTEREST_RATE = _1pct / 2; // 0.5%
-uint256 constant MAX_ANNUAL_INTEREST_RATE = 250 * _1pct;
-
-// Batch management params
-uint128 constant MAX_ANNUAL_BATCH_MANAGEMENT_FEE = uint128(_100pct / 10); // 10%
-uint128 constant MIN_INTEREST_RATE_CHANGE_PERIOD = 1 hours; // only applies to batch managers / batched Troves
-
-uint256 constant REDEMPTION_FEE_FLOOR = _1pct / 2; // 0.5%
-
-// For the debt / shares ratio to increase by a factor 1e9
-// at a average annual debt increase (compounded interest + fees) of 10%, it would take more than 217 years (log(1e9)/log(1.1))
-// at a average annual debt increase (compounded interest + fees) of 50%, it would take more than 51 years (log(1e9)/log(1.5))
-// The increase pace could be forced to be higher through an inflation attack,
-// but precisely the fact that we have this max value now prevents the attack
-uint256 constant MAX_BATCH_SHARES_RATIO = 1e9;
-
-// Half-life of 6h. 6h = 360 min
-// (1/2) = d^360 => d = (1/2)^(1/360)
-uint256 constant REDEMPTION_MINUTE_DECAY_FACTOR = 998076443575628800;
-
-// BETA: 18 digit decimal. Parameter by which to divide the redeemed fraction, in order to calc the new base rate from a redemption.
-// Corresponds to (1 / ALPHA) in the white paper.
-uint256 constant REDEMPTION_BETA = 1;
-
-// To prevent redemptions unless Bold depegs below 0.95 and allow the system to take off
-uint256 constant INITIAL_BASE_RATE = _100pct; // 100% initial redemption rate
-
-// Discount to be used once the shutdown thas been triggered
-uint256 constant URGENT_REDEMPTION_BONUS = 2e16; // 2%
-
 uint256 constant ONE_MINUTE = 1 minutes;
 uint256 constant ONE_YEAR = 365 days;
-uint256 constant UPFRONT_INTEREST_PERIOD = 7 days;
-uint256 constant INTEREST_RATE_ADJ_COOLDOWN = 7 days;
 
-uint256 constant SP_YIELD_SPLIT = 75 * _1pct; // 75%
 
-uint256 constant MIN_BOLD_IN_SP = 1e18;
-
+// TODO(@bayological): Remve this and refactor the tests that use it
 // Dummy contract that lets legacy Hardhat tests query some of the constants
 contract Constants {
     uint256 public constant _ETH_GAS_COMPENSATION = ETH_GAS_COMPENSATION;
