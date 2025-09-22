69e650fb9736208b47560e7d80fbbee68400bc7f
geovgy <54918343+geovgy@users.noreply.github.com>
2025-09-22 09:54:33 -0400

    Add banner for promo

---
+++ frontend/app/Banner.tsx
@@ -0,0 +1,47 @@
+// export function Banner() {
+//   return null;
+// }
+
+// Example banner:
+
+import { css } from "@/styled-system/css";
+// import { AnchorTextButton } from "@liquity2/uikit";
+// import Link from "next/link";
+
+export function Banner() {
+  return (
+    <div
+      className={css({
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        maxWidth: "100%",
+        width: "100%",
+        height: 40,
+        textAlign: "center",
+        color: "#fff",
+        background: "#2f4225",
+      })}
+    >
+      <div
+        className={css({
+          width: "100%",
+          maxWidth: 1092,
+        })}
+      >
+        It's Arbitrum Week! Earn 5x daily bonus in Shells if you borrow against ARB between Sept. 22 and Sept. 29.
+        {/* <Link
+          href="https://example.com"
+          passHref
+          legacyBehavior
+        >
+          <AnchorTextButton
+            external
+            label="link example"
+            className={css({ color: "inherit" })}
+          />
+        </Link>. */}
+      </div>
+    </div>
+  );
+}
