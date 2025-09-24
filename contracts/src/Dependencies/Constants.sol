26db118be909b59cd83cf06651d436b15350125a
Bayological <6872903+bayological@users.noreply.github.com>
2025-09-24 13:46:52 -0500

    chore: remove unused constants

--- contracts/src/Dependencies/Constants.sol
+++ contracts/src/Dependencies/Constants.sol
@@ -11,11 +11,3 @@ uint256 constant _1pct = DECIMAL_PRECISION / 100;
 
 uint256 constant ONE_MINUTE = 1 minutes;
 uint256 constant ONE_YEAR = 365 days;
-
-
-// TODO(@bayological): Remve this and refactor the tests that use it
-// Dummy contract that lets legacy Hardhat tests query some of the constants
-contract Constants {
-    uint256 public constant _ETH_GAS_COMPENSATION = 123;
-    uint256 public constant _MIN_DEBT = 123;
-}
