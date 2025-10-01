b2888ab8d8465d8640660d77047cf6da249c36eb
Bayological <6872903+bayological@users.noreply.github.com>
2025-10-01 15:37:02 -0500

    feat: set params via constructor

--- contracts/src/Interfaces/ISystemParams.sol
+++ contracts/src/Interfaces/ISystemParams.sol
@@ -3,6 +3,53 @@
 pragma solidity 0.8.24;
 
 interface ISystemParams {
+    /* ========== PARAMETER STRUCTS ========== */
+
+    struct DebtParams {
+        uint256 minDebt;
+    }
+
+    struct LiquidationParams {
+        uint256 liquidationPenaltySP;
+        uint256 liquidationPenaltyRedistribution;
+    }
+
+    struct GasCompParams {
+        uint256 collGasCompensationDivisor;
+        uint256 collGasCompensationCap;
+        uint256 ethGasCompensation;
+    }
+
+    struct CollateralParams {
+        uint256 ccr;
+        uint256 scr;
+        uint256 mcr;
+        uint256 bcr;
+    }
+
+    struct InterestParams {
+        uint256 minAnnualInterestRate;
+        uint256 maxAnnualInterestRate;
+        uint128 maxAnnualBatchManagementFee;
+        uint256 upfrontInterestPeriod;
+        uint256 interestRateAdjCooldown;
+        uint128 minInterestRateChangePeriod;
+        uint256 maxBatchSharesRatio;
+    }
+
+    struct RedemptionParams {
+        uint256 redemptionFeeFloor;
+        uint256 initialBaseRate;
+        uint256 redemptionMinuteDecayFactor;
+        uint256 redemptionBeta;
+        uint256 urgentRedemptionBonus;
+    }
+
+    struct StabilityPoolParams {
+        uint256 spYieldSplit;
+        uint256 minBoldInSP;
+    }
+
     /* ========== ERRORS ========== */
 
     error InvalidMinDebt();
@@ -19,76 +66,12 @@ interface ISystemParams {
     error SPPenaltyGtRedist();
     error RedistPenaltyTooHigh();
 
-    /* ========== EVENTS ========== */
-
-    /// @notice Emitted when min debt is updated.
-    event MinDebtUpdated(uint256 oldMinDebt, uint256 newMinDebt);
-
-    /// @notice Emitted when liquidation parameters are updated.
-    event LiquidationParamsUpdated(
-        uint256 minLiquidationPenaltySP,
-        uint256 maxLiquidationPenaltyRedistribution,
-        uint256 liquidationPenaltySP,
-        uint256 liquidationPenaltyRedistribution
-    );
-
-    /// @notice Emitted when gas compensation parameters are updated.
-    event GasCompParamsUpdated(
-        uint256 collGasCompensationDivisor,
-        uint256 collGasCompensationCap,
-        uint256 ethGasCompensation
-    );
-
-    /// @notice Emitted when collateral parameters are updated.
-    event CollateralParamsUpdated(
-        uint256 ccr,
-        uint256 scr,
-        uint256 mcr,
-        uint256 bcr
-    );
-
-    /// @notice Emitted when Interest rate parameters are updated.
-    event InterestParamsUpdated(
-        uint256 minAnnualInterestRate,
-        uint256 maxAnnualInterestRate,
-        uint128 maxAnnualBatchManagementFee,
-        uint256 upfrontInterestPeriod,
-        uint256 interestRateAdjCooldown,
-        uint128 minInterestRateChangePeriod,
-        uint256 maxBatchSharesRatio
-    );
-
-    /// @notice Emitted when redemption parameters are updated.
-    event RedemptionParamsUpdated(
-        uint256 redemptionFeeFloor,
-        uint256 initialBaseRate,
-        uint256 redemptionMinuteDecayFactor,
-        uint256 redemptionBeta,
-        uint256 urgentRedemptionBonus
-    );
-
-    /// @notice Emitted when stability pool parameters are updated.
-    event StabilityPoolParamsUpdated(uint256 spYieldSplit, uint256 minBoldInSP);
-
-
-    /// @notice Emitted when the version is updated.
-    event VersionUpdated(uint256 oldVersion, uint256 newVersion);
-
     /* ========== DEBT PARAMETERS ========== */
 
     /// @notice Minimum amount of net debt a trove must have.
     function MIN_DEBT() external view returns (uint256);
 
     /* ========== LIQUIDATION PARAMETERS ========== */
-
-    // TODO(@bayological): These min and max params are used to validate
-    //                     the penalaties are within expected ranges when
-    //                     being set. Do we want these to be configurable?
-    /// @notice Minimum liquidation penalty for troves offset to the SP
-    function MIN_LIQUIDATION_PENALTY_SP() external view returns (uint256);
-
-    /// @notice Maximum liquidation penalty for troves redistributed.
-    function MAX_LIQUIDATION_PENALTY_REDISTRIBUTION() external view returns (uint256);
 
     /// @notice Liquidation penalty for troves offset to the SP
     function LIQUIDATION_PENALTY_SP() external view returns (uint256);
@@ -186,110 +169,4 @@ interface ISystemParams {
     /// @notice Minimum BOLD that must remain in Stability Pool to prevent complete drainage.
     function MIN_BOLD_IN_SP() external view returns (uint256);
 
-    /* ========== VERSION ========== */
-
-     /// @notice Returns the version of the system params contract.
-    function version() external view returns (uint256);
-
-    /* ========== FUNCTIONS ========== */
-
-    /**
-     * @notice Update the minimum debt
-     * @param _minDebt The new minimum debt amount
-     */
-    function updateMinDebt(
-        uint256 _minDebt
-    ) external;
-
-    /**
-     * @notice Update the liquidation params.
-     * @param _minLiquidationPenaltySP The minimum liquidation penalty for stability pool
-     * @param _maxLiquidationPenaltyRedistribution The maximum liquidation penalty for redistribution
-     * @param _liquidationPenaltySP The liquidation penalty for stability pool
-     * @param _liquidationPenaltyRedistribution The liquidation penalty for redistribution
-     */
-    function updateLiquidationParams(
-        uint256 _minLiquidationPenaltySP,
-        uint256 _maxLiquidationPenaltyRedistribution,
-        uint256 _liquidationPenaltySP,
-        uint256 _liquidationPenaltyRedistribution
-    ) external;
-
-    /**
-     * @notice Update gas compensation parameters.
-     * @param _collGasCompensationDivisor Collateral gas compensation divisor.
-     * @param _collGasCompensationCap Collateral gas compensation cap.
-     * @param _ethGasCompensation Amount of ETH to be locked in gas pool on opening troves.
-     */
-    function updateGasCompParams(
-        uint256 _collGasCompensationDivisor,
-        uint256 _collGasCompensationCap,
-        uint256 _ethGasCompensation
-    ) external;
-
-    /**
-     * @notice Update the collateral related parameters.
-     * @param _ccr The critical collateral ratio.
-     * @param _scr The shutdown collateral ratio.
-     * @param _mcr The minimum collateral ratio.
-     * @param _bcr The base collateral ratio.
-     */
-    function updateCollateralParams(
-        uint256 _ccr,
-        uint256 _scr,
-        uint256 _mcr,
-        uint256 _bcr
-    ) external;
-
-    /**
-     * @notice Update interest related parameters.
-     * @param _minAnnualInterestRate The minimum annual interest rate
-     * @param _maxAnnualInterestRate The maximum annual interest rate
-     * @param _maxAnnualBatchManagementFee The maximum annual batch management fee
-     * @param _upfrontInterestPeriod The upfront interest period
-     * @param _interestRateAdjCooldown The interest rate adjustment cooldown
-     * @param _minInterestRateChangePeriod The minimum interest rate change period
-     * @param _maxBatchSharesRatio The maximum ratio between batch debt and shares to prevent inflation attacks
-     */
-    function updateInterestParams(
-        uint256 _minAnnualInterestRate,
-        uint256 _maxAnnualInterestRate,
-        uint128 _maxAnnualBatchManagementFee,
-        uint256 _upfrontInterestPeriod,
-        uint256 _interestRateAdjCooldown,
-        uint128 _minInterestRateChangePeriod,
-        uint256 _maxBatchSharesRatio
-    ) external;
-
-    /**
-     * @notice Update redemption related parameters.
-     * @param _redemptionFeeFloor The min redemption fee percentage
-     * @param _initialBaseRate The initial base rate
-     * @param _redemptionMinuteDecayFactor Factor to reduce the redemption fee per minute.
-     * @param _redemptionBeta The redemption beta
-     * @param _urgentRedemptionBonus The urgent redemption bonus given to redeemers after shutdownn
-     */
-    function updateRedemptionParams(
-        uint256 _redemptionFeeFloor,
-        uint256 _initialBaseRate,
-        uint256 _redemptionMinuteDecayFactor,
-        uint256 _redemptionBeta,
-        uint256 _urgentRedemptionBonus
-    ) external;
-    
-    /**
-     * @notice Update stability pool related parameters.
-     * @param _spYieldSplit Percentage of minted interest yield allocated to Stability Pool depositors.
-     * @param _minBoldInSP Minimum BOLD that must remain in Stability Pool to prevent complete drainage.
-     */
-    function updatePoolParams(
-        uint256 _spYieldSplit,
-        uint256 _minBoldInSP
-    ) external;
-
-    /**
-     * @notice Update the version of the system params contract.
-     * @param _newVersion The new version number.
-     */
-    function updateVersion(uint256 _newVersion) external;
 }
--- contracts/src/SystemParams.sol
+++ contracts/src/SystemParams.sol
@@ -2,9 +2,6 @@
 
 pragma solidity 0.8.24;
 
-import {OwnableUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/access/OwnableUpgradeable.sol";
-import {Initializable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
-
 import {ISystemParams} from "./Interfaces/ISystemParams.sol";
 import {_100pct, _1pct} from "./Dependencies/Constants.sol";
 
@@ -13,17 +10,13 @@ import {_100pct, _1pct} from "./Dependencies/Constants.sol";
  * @author Mento Labs
  * @notice This contract manages the system-wide parameters for the protocol.
  */
-contract SystemParams is Initializable, OwnableUpgradeable, ISystemParams {
-    uint256 public version;
-
+contract SystemParams is ISystemParams {
     /* ========== DEBT PARAMETERS ========== */
 
     uint256 public MIN_DEBT;
 
     /* ========== LIQUIDATION PARAMETERS ========== */
 
-    uint256 public MIN_LIQUIDATION_PENALTY_SP;
-    uint256 public MAX_LIQUIDATION_PENALTY_REDISTRIBUTION;
     uint256 public LIQUIDATION_PENALTY_SP;
     uint256 public LIQUIDATION_PENALTY_REDISTRIBUTION;
 
@@ -65,247 +58,105 @@ contract SystemParams is Initializable, OwnableUpgradeable, ISystemParams {
 
     /* ========== CONSTRUCTOR ========== */
 
-    /**
-     * @notice Contract constructor
-     * @param disable Boolean to disable initializers for implementation contract
-     */
-    constructor(bool disable) {
-        if (disable) {
-            _disableInitializers();
-        }
-    }
+    constructor(
+        DebtParams memory _debtParams,
+        LiquidationParams memory _liquidationParams,
+        GasCompParams memory _gasCompParams,
+        CollateralParams memory _collateralParams,
+        InterestParams memory _interestParams,
+        RedemptionParams memory _redemptionParams,
+        StabilityPoolParams memory _poolParams
+    ) {
+        // Validate debt parameters
+        if (_debtParams.minDebt == 0 || _debtParams.minDebt > 10000e18) revert InvalidMinDebt();
 
-    /* ========== INITIALIZATION ========== */
-
-    function initialize(address owner_) public initializer {
-        __Ownable_init();
-        transferOwnership(owner_);
-
-        // Debt parameters
-        MIN_DEBT = 2000e18;
-
-        // Liquidation parameters
-        MIN_LIQUIDATION_PENALTY_SP = 5 * _1pct; // 5%
-        MAX_LIQUIDATION_PENALTY_REDISTRIBUTION = 20 * _1pct; // 20%
-        LIQUIDATION_PENALTY_SP = 5e16; // 5%
-        LIQUIDATION_PENALTY_REDISTRIBUTION = 10e16; // 10%
-
-        // Gas compensation parameters
-        COLL_GAS_COMPENSATION_DIVISOR = 200; // dividing by 200 yields 0.5%
-        COLL_GAS_COMPENSATION_CAP = 2 ether; // Max coll gas compensation capped at 2 ETH
-        ETH_GAS_COMPENSATION = 0.0375 ether;
-
-        // Collateral parameters
-        CCR = 150 * _1pct; // 150%
-        SCR = 110 * _1pct; // 110%
-        MCR = 110 * _1pct; // 110%
-        BCR = 10 * _1pct; // 10%
-
-        // Interest parameters
-        MIN_ANNUAL_INTEREST_RATE = _1pct / 2; // 0.5%
-        MAX_ANNUAL_INTEREST_RATE = 250 * _1pct; // 250%
-        MAX_ANNUAL_BATCH_MANAGEMENT_FEE = uint128(_100pct / 10); // 10%
-        UPFRONT_INTEREST_PERIOD = 7 days;
-        INTEREST_RATE_ADJ_COOLDOWN = 7 days;
-        MIN_INTEREST_RATE_CHANGE_PERIOD = 1 hours; // only applies to batch managers / batched Troves
-        MAX_BATCH_SHARES_RATIO = 1e9;
-
-        // Redemption parameters
-        REDEMPTION_FEE_FLOOR = _1pct / 2; // 0.5%
-        INITIAL_BASE_RATE = _100pct; // 100% initial redemption rate
-
-        // Half-life of 6h. 6h = 360 min
-        // (1/2) = d^360 => d = (1/2)^(1/360)
-        REDEMPTION_MINUTE_DECAY_FACTOR = 998076443575628800;
-        REDEMPTION_BETA = 1;
-        URGENT_REDEMPTION_BONUS = 2 * _1pct; // 2%
-
-        // Stability pool parameters
-        SP_YIELD_SPLIT = 75 * _1pct; // 75%
-        MIN_BOLD_IN_SP = 1e18;
-
-        version = 1;
-        emit VersionUpdated(0, 1);
-    }
-
-    /* ========== ADMIN FUNCTIONS ========== */
-
-    /// @inheritdoc ISystemParams
-    function updateMinDebt(uint256 _minDebt) external onlyOwner {
-        if (_minDebt == 0 || _minDebt > 10000e18) revert InvalidMinDebt();
-
-        uint256 oldMinDebt = MIN_DEBT;
-        MIN_DEBT = _minDebt;
-
-        emit MinDebtUpdated(oldMinDebt, _minDebt);
-    }
-
-    /// @inheritdoc ISystemParams
-    function updateLiquidationParams(
-        uint256 _minLiquidationPenaltySP,
-        uint256 _maxLiquidationPenaltyRedistribution,
-        uint256 _liquidationPenaltySP,
-        uint256 _liquidationPenaltyRedistribution
-    ) external onlyOwner {
-        // TODO: Review checks.
-        if (_liquidationPenaltySP < _minLiquidationPenaltySP)
+        // Validate liquidation parameters
+        // Hardcoded validation bounds: MIN_LIQUIDATION_PENALTY_SP = 5%, MAX_LIQUIDATION_PENALTY_REDISTRIBUTION = 20%
+        if (_liquidationParams.liquidationPenaltySP < 5 * _1pct)
             revert SPPenaltyTooLow();
-        if (_liquidationPenaltySP > _liquidationPenaltyRedistribution)
+        if (_liquidationParams.liquidationPenaltySP > _liquidationParams.liquidationPenaltyRedistribution)
             revert SPPenaltyGtRedist();
+        if (_liquidationParams.liquidationPenaltyRedistribution > 20 * _1pct)
+            revert RedistPenaltyTooHigh();
+
+        // Validate gas compensation parameters
         if (
-            _liquidationPenaltyRedistribution >
-            _maxLiquidationPenaltyRedistribution
-        ) revert RedistPenaltyTooHigh();
-
-        MIN_LIQUIDATION_PENALTY_SP = _minLiquidationPenaltySP;
-        MAX_LIQUIDATION_PENALTY_REDISTRIBUTION = _maxLiquidationPenaltyRedistribution;
-        LIQUIDATION_PENALTY_SP = _liquidationPenaltySP;
-        LIQUIDATION_PENALTY_REDISTRIBUTION = _liquidationPenaltyRedistribution;
-
-        emit LiquidationParamsUpdated(
-            _minLiquidationPenaltySP,
-            _maxLiquidationPenaltyRedistribution,
-            _liquidationPenaltySP,
-            _liquidationPenaltyRedistribution
-        );
-    }
-
-    /// @inheritdoc ISystemParams
-    function updateGasCompParams(
-        uint256 _collGasCompensationDivisor,
-        uint256 _collGasCompensationCap,
-        uint256 _ethGasCompensation
-    ) external onlyOwner {
-        if (
-            _collGasCompensationDivisor == 0 ||
-            _collGasCompensationDivisor > 1000
+            _gasCompParams.collGasCompensationDivisor == 0 ||
+            _gasCompParams.collGasCompensationDivisor > 1000
         ) revert InvalidGasCompensation();
-        if (_collGasCompensationCap == 0 || _collGasCompensationCap > 10 ether)
+        if (_gasCompParams.collGasCompensationCap == 0 || _gasCompParams.collGasCompensationCap > 10 ether)
             revert InvalidGasCompensation();
-        if (_ethGasCompensation == 0 || _ethGasCompensation > 1 ether)
+        if (_gasCompParams.ethGasCompensation == 0 || _gasCompParams.ethGasCompensation > 1 ether)
             revert InvalidGasCompensation();
 
-        COLL_GAS_COMPENSATION_DIVISOR = _collGasCompensationDivisor;
-        COLL_GAS_COMPENSATION_CAP = _collGasCompensationCap;
-        ETH_GAS_COMPENSATION = _ethGasCompensation;
+        // Validate collateral parameters
+        if (_collateralParams.ccr <= _100pct || _collateralParams.ccr >= 2 * _100pct) revert InvalidCCR();
+        if (_collateralParams.mcr <= _100pct || _collateralParams.mcr >= 2 * _100pct) revert InvalidMCR();
+        if (_collateralParams.bcr < 5 * _1pct || _collateralParams.bcr >= 50 * _1pct) revert InvalidBCR();
+        if (_collateralParams.scr <= _100pct || _collateralParams.scr >= 2 * _100pct) revert InvalidSCR();
 
-        emit GasCompParamsUpdated(
-            _collGasCompensationDivisor,
-            _collGasCompensationCap,
-            _ethGasCompensation
-        );
-    }
-
-    /// @inheritdoc ISystemParams
-    function updateCollateralParams(
-        uint256 _ccr,
-        uint256 _scr,
-        uint256 _mcr,
-        uint256 _bcr
-    ) external onlyOwner {
-        if (_ccr <= _100pct || _ccr >= 2 * _100pct) revert InvalidCCR();
-        if (_mcr <= _100pct || _mcr >= 2 * _100pct) revert InvalidMCR();
-        if (_bcr < 5 * _1pct || _bcr >= 50 * _1pct) revert InvalidBCR();
-        if (_scr <= _100pct || _scr >= 2 * _100pct) revert InvalidSCR();
-
-        CCR = _ccr;
-        SCR = _scr;
-        MCR = _mcr;
-        BCR = _bcr;
-
-        emit CollateralParamsUpdated(_ccr, _scr, _mcr, _bcr);
-    }
-
-    /// @inheritdoc ISystemParams
-    function updateInterestParams(
-        uint256 _minAnnualInterestRate,
-        uint256 _maxAnnualInterestRate,
-        uint128 _maxAnnualBatchManagementFee,
-        uint256 _upfrontInterestPeriod,
-        uint256 _interestRateAdjCooldown,
-        uint128 _minInterestRateChangePeriod,
-        uint256 _maxBatchSharesRatio
-    ) external onlyOwner {
-        if (_minAnnualInterestRate > _maxAnnualInterestRate)
+        // Validate interest parameters
+        if (_interestParams.minAnnualInterestRate > _interestParams.maxAnnualInterestRate)
             revert MinInterestRateGtMax();
-        if (_maxAnnualInterestRate > 10 * _100pct)
+        if (_interestParams.maxAnnualInterestRate > 10 * _100pct)
             revert InvalidInterestRateBounds(); // Max 1000%
-        if (_maxAnnualBatchManagementFee > _100pct) revert InvalidFeeValue();
+        if (_interestParams.maxAnnualBatchManagementFee > _100pct) revert InvalidFeeValue();
 
-        if (_upfrontInterestPeriod == 0 || _upfrontInterestPeriod > 365 days)
+        if (_interestParams.upfrontInterestPeriod == 0 || _interestParams.upfrontInterestPeriod > 365 days)
             revert InvalidTimeValue();
         if (
-            _interestRateAdjCooldown == 0 || _interestRateAdjCooldown > 365 days
+            _interestParams.interestRateAdjCooldown == 0 || _interestParams.interestRateAdjCooldown > 365 days
         ) revert InvalidTimeValue();
         if (
-            _minInterestRateChangePeriod == 0 ||
-            _minInterestRateChangePeriod > 30 days
+            _interestParams.minInterestRateChangePeriod == 0 ||
+            _interestParams.minInterestRateChangePeriod > 30 days
         ) revert InvalidTimeValue();
 
-        MIN_ANNUAL_INTEREST_RATE = _minAnnualInterestRate;
-        MAX_ANNUAL_INTEREST_RATE = _maxAnnualInterestRate;
-        MAX_ANNUAL_BATCH_MANAGEMENT_FEE = _maxAnnualBatchManagementFee;
-        UPFRONT_INTEREST_PERIOD = _upfrontInterestPeriod;
-        INTEREST_RATE_ADJ_COOLDOWN = _interestRateAdjCooldown;
-        MIN_INTEREST_RATE_CHANGE_PERIOD = _minInterestRateChangePeriod;
-        MAX_BATCH_SHARES_RATIO = _maxBatchSharesRatio;
+        // Validate redemption parameters
+        if (_redemptionParams.redemptionFeeFloor > _100pct) revert InvalidFeeValue();
+        if (_redemptionParams.initialBaseRate > 10 * _100pct) revert InvalidFeeValue();
+        if (_redemptionParams.urgentRedemptionBonus > _100pct) revert InvalidFeeValue();
 
-        emit InterestParamsUpdated(
-            _minAnnualInterestRate,
-            _maxAnnualInterestRate,
-            _maxAnnualBatchManagementFee,
-            _upfrontInterestPeriod,
-            _interestRateAdjCooldown,
-            _minInterestRateChangePeriod,
-            _maxBatchSharesRatio
-        );
-    }
+        // Validate stability pool parameters
+        if (_poolParams.spYieldSplit > _100pct) revert InvalidFeeValue();
+        if (_poolParams.minBoldInSP == 0) revert InvalidMinDebt();
 
-    /// @inheritdoc ISystemParams
-    function updateRedemptionParams(
-        uint256 _redemptionFeeFloor,
-        uint256 _initialBaseRate,
-        uint256 _redemptionMinuteDecayFactor,
-        uint256 _redemptionBeta,
-        uint256 _urgentRedemptionBonus
-    ) external onlyOwner {
-        if (_redemptionFeeFloor > _100pct) revert InvalidFeeValue();
-        if (_initialBaseRate > 10 * _100pct) revert InvalidFeeValue();
-        if (_urgentRedemptionBonus > _100pct) revert InvalidFeeValue();
+        // Set debt parameters
+        MIN_DEBT = _debtParams.minDebt;
 
-        REDEMPTION_FEE_FLOOR = _redemptionFeeFloor;
-        INITIAL_BASE_RATE = _initialBaseRate;
-        REDEMPTION_MINUTE_DECAY_FACTOR = _redemptionMinuteDecayFactor;
-        REDEMPTION_BETA = _redemptionBeta;
-        URGENT_REDEMPTION_BONUS = _urgentRedemptionBonus;
+        // Set liquidation parameters
+        LIQUIDATION_PENALTY_SP = _liquidationParams.liquidationPenaltySP;
+        LIQUIDATION_PENALTY_REDISTRIBUTION = _liquidationParams.liquidationPenaltyRedistribution;
 
-        emit RedemptionParamsUpdated(
-            _redemptionFeeFloor,
-            _initialBaseRate,
-            _redemptionMinuteDecayFactor,
-            _redemptionBeta,
-            _urgentRedemptionBonus
-        );
-    }
+        // Set gas compensation parameters
+        COLL_GAS_COMPENSATION_DIVISOR = _gasCompParams.collGasCompensationDivisor;
+        COLL_GAS_COMPENSATION_CAP = _gasCompParams.collGasCompensationCap;
+        ETH_GAS_COMPENSATION = _gasCompParams.ethGasCompensation;
 
-    /// @inheritdoc ISystemParams
-    function updatePoolParams(
-        uint256 _spYieldSplit,
-        uint256 _minBoldInSP
-    ) external onlyOwner {
-        if (_spYieldSplit > _100pct) revert InvalidFeeValue();
-        if (_minBoldInSP == 0) revert InvalidMinDebt();
+        // Set collateral parameters
+        CCR = _collateralParams.ccr;
+        SCR = _collateralParams.scr;
+        MCR = _collateralParams.mcr;
+        BCR = _collateralParams.bcr;
 
-        SP_YIELD_SPLIT = _spYieldSplit;
-        MIN_BOLD_IN_SP = _minBoldInSP;
+        // Set interest parameters
+        MIN_ANNUAL_INTEREST_RATE = _interestParams.minAnnualInterestRate;
+        MAX_ANNUAL_INTEREST_RATE = _interestParams.maxAnnualInterestRate;
+        MAX_ANNUAL_BATCH_MANAGEMENT_FEE = _interestParams.maxAnnualBatchManagementFee;
+        UPFRONT_INTEREST_PERIOD = _interestParams.upfrontInterestPeriod;
+        INTEREST_RATE_ADJ_COOLDOWN = _interestParams.interestRateAdjCooldown;
+        MIN_INTEREST_RATE_CHANGE_PERIOD = _interestParams.minInterestRateChangePeriod;
+        MAX_BATCH_SHARES_RATIO = _interestParams.maxBatchSharesRatio;
 
-        emit StabilityPoolParamsUpdated(_spYieldSplit, _minBoldInSP);
-    }
+        // Set redemption parameters
+        REDEMPTION_FEE_FLOOR = _redemptionParams.redemptionFeeFloor;
+        INITIAL_BASE_RATE = _redemptionParams.initialBaseRate;
+        REDEMPTION_MINUTE_DECAY_FACTOR = _redemptionParams.redemptionMinuteDecayFactor;
+        REDEMPTION_BETA = _redemptionParams.redemptionBeta;
+        URGENT_REDEMPTION_BONUS = _redemptionParams.urgentRedemptionBonus;
 
-    /// @inheritdoc ISystemParams
-    function updateVersion(uint256 newVersion) external onlyOwner {
-        uint256 oldVersion = version;
-        version = newVersion;
-        emit VersionUpdated(oldVersion, newVersion);
+        // Set stability pool parameters
+        SP_YIELD_SPLIT = _poolParams.spYieldSplit;
+        MIN_BOLD_IN_SP = _poolParams.minBoldInSP;
     }
 }
