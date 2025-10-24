f9d1d38eb6a5f75a58a4bee091e8b34b14e39061
Daniel Simon <daniel.attila.simon@gmail.com>
2025-10-24 19:57:50 +0700

    chore: add some basic notes on function usage

--- contracts/src/Interfaces/IRedemptionHelper.sol
+++ contracts/src/Interfaces/IRedemptionHelper.sol
@@ -22,10 +22,20 @@ interface IRedemptionHelper {
         external
         returns (SimulationContext[] memory branch, uint256 totalProportions);
 
+    // Find the maximal amount of BOLD that can be redeemed proportionally within
+    // a given iteration limit. This helps prevent the redeemer from overpaying on
+    // the redemption fee.
+    //
+    // Also returns the expected fee that will be paid (as a percentage), and the
+    // expected collateral amounts that will be paid out in exchange for the
+    // redeemed BOLD. The latter may be used to calculate the _minCollRedeemed
+    // parameter passed to redeemCollateral().
     function truncateRedemption(uint256 _bold, uint256 _maxIterationsPerCollateral)
         external
         returns (uint256 truncatedBold, uint256 feePct, Redeemed[] memory redeemed);
 
+    // Wrapper around CollateralRegistry's redeemCollateral() that adds slippage
+    // protection in the form of a minimum acceptable collateral amounts parameter.
     function redeemCollateral(
         uint256 _bold,
         uint256 _maxIterationsPerCollateral,
